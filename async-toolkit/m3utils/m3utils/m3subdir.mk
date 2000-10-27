4e79cf5b3e221d52a1aa3cc89abb7e668e69e41e
Mika Nystrom <mika@alum.mit.edu>
2000-10-27 01:35:33 +0000

    Fixed null command /bin/sh problem.

--- async-toolkit/m3utils/m3utils/m3subdir.mk
+++ async-toolkit/m3utils/m3utils/m3subdir.mk
@@ -1,6 +1,8 @@
 # $Id$ 
+PRE_MAKE?=echo
+PRE_MAKE_CLEAN?=echo
 all: 
 	cd src; $(PRE_MAKE); m3build; cd ..
 
 clean:
-	cd src; $(PRE_MAKE_CLEAN); m3build clean: cd ..
+	cd src; $(PRE_MAKE_CLEAN); m3build clean; cd ..
