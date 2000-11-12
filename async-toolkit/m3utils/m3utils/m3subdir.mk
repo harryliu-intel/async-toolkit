6c54f946ac4a94db6cc42ed2a5edbd33c056c3c6
Mika Nystrom <mika@alum.mit.edu>
2000-11-12 00:22:05 +0000

    Made it possible to build everything w/o shipping first.

--- async-toolkit/m3utils/m3utils/m3subdir.mk
+++ async-toolkit/m3utils/m3utils/m3subdir.mk
@@ -2,7 +2,7 @@
 PRE_MAKE?=echo
 PRE_MAKE_CLEAN?=echo
 all: 
-	cd src; $(PRE_MAKE); m3build; cd ..
+	cd src; $(PRE_MAKE); m3build $(M3BUILDOPTS); cd ..
 
 clean:
-	cd src; $(PRE_MAKE_CLEAN); m3build clean; cd ..
+	cd src; $(PRE_MAKE_CLEAN); m3build $(M3BUILDOPTS) clean || true ; cd ..
