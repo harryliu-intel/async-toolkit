de75042588467d87c216d30eb7a06ce715ec7931
Mika Nystrom <mika@alum.mit.edu>
2000-11-12 05:15:15 +0000

    *** empty log message ***

--- async-toolkit/m3utils/m3utils/dirsexist.sh
+++ async-toolkit/m3utils/m3utils/dirsexist.sh
@@ -4,6 +4,8 @@
 # dirsexist.sh -- take $* and print it, but only for those words that are
 # existing directories
 
+PRINTDIRECTION=$1
+shift
 DIRS=$*
 DIRSEXIST=""
 
@@ -12,4 +14,13 @@ for dir in $DIRS; do
 		DIRSEXIST="${DIRSEXIST} ${dir}"
 	fi
 done
-echo ${DIRSEXIST}
+
+if [ "X${PRINTDIRECTION}" = "X-f" ]; then
+	echo ${DIRSEXIST}
+elif [ "X${PRINTDIRECTION}" = "X-r" ]; then 
+	echo ${DIRSEXIST} | xargs -n1 echo | tail -r | xargs echo
+else
+	echo "ERROR, wrong args"
+	exit 1
+fi
+
