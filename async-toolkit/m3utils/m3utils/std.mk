0b2f73d7501966e4ed488f17ffd8a9cc8b5e815f
Mika Nystrom <mika@alum.mit.edu>
2004-07-14 02:10:05 +0000

    got rid of -Wtraditional

--- async-toolkit/m3utils/m3utils/std.mk
+++ async-toolkit/m3utils/m3utils/std.mk
@@ -5,7 +5,7 @@
 
 DEBUGFLAGS= -g 
 #DEBUGFLAGS= -O6
-CFLAGS= $(DEBUGFLAGS) -Wall -Wtraditional -Wshadow -Wpointer-arith -Wcast-qual -Wcast-align -Wwrite-strings -Wconversion -Waggregate-return -Wstrict-prototypes -Wnested-externs $(ANNOYINGFLAGS) -pipe -DMYMALLOCDEBUG 
+CFLAGS= $(DEBUGFLAGS) -Wall -Wshadow -Wpointer-arith -Wcast-qual -Wcast-align -Wwrite-strings -Wconversion -Waggregate-return -Wstrict-prototypes -Wnested-externs $(ANNOYINGFLAGS) -pipe -DMYMALLOCDEBUG 
 
 #CFLAGS+= -DQUIET
 #MCCFLAGS+= -DQUIET
