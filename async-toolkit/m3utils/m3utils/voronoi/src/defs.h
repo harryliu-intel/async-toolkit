6b27e2c6762cc8e952cf8b409cb536b26e95c700
Mika Nystrom <mika@alum.mit.edu>
2004-10-28 15:18:53 +0000

    added voronoi and made many sysiphus changes...

---
+++ async-toolkit/m3utils/m3utils/voronoi/src/defs.h
@@ -0,0 +1,86 @@
+#ifndef NULL
+#define NULL 0
+#endif
+#define DELETED -2
+
+int triangulate, sorted, plot, debug;
+
+
+float xmin, xmax, ymin, ymax, deltax, deltay;
+
+
+struct Point	{
+float x,y;
+};
+
+/* structure used both for sites and for vertices */
+struct Site	{
+struct	Point	coord;
+int		sitenbr;
+int		refcnt;
+};
+
+
+struct	Site	*sites;
+int		nsites;
+int		siteidx;
+int		sqrt_nsites;
+int		nvertices;
+struct	Site	*bottomsite;
+
+
+struct TripleArg {
+  int s1, s2, s3;
+};
+
+struct Triple {
+  struct TripleArg d;
+  struct Triple *next;
+};
+
+struct Triple *triples;
+
+
+struct Edge	{
+float		a,b,c;
+struct	Site 	*ep[2];
+struct	Site	*reg[2];
+int		edgenbr;
+};
+#define le 0
+#define re 1
+int nedges;
+
+int has_endpoint(),right_of();
+struct Site *intersect();
+float dist();
+struct Point PQ_min();
+struct Halfedge *PQextractmin();
+struct Edge *bisect();
+
+struct Halfedge {
+struct Halfedge	*ELleft, *ELright;
+struct Edge	*ELedge;
+int		ELrefcnt;
+char		ELpm;
+struct	Site	*vertex;
+float		ystar;
+struct	Halfedge *PQnext;
+};
+
+struct	Halfedge *ELleftend, *ELrightend;
+int 	ELhashsize;
+struct	Halfedge **ELhash;
+struct	Halfedge *HEcreate(), *ELleft(), *ELright(), *ELleftbnd();
+struct	Site *leftreg(), *rightreg();
+
+
+int PQhashsize;
+struct	Halfedge *PQhash;
+struct	Halfedge *PQfind();
+int PQcount;
+int PQmin;
+int PQempty();
+
+char *memmalloc();
+
---
+++ async-toolkit/m3utils/m3utils/voronoi/src/mod3_main.c
@@ -0,0 +1,187 @@
+/*
+ * The author of this software is Steven Fortune.  Copyright (c) 1994 by AT&T
+ * Bell Laboratories.
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose without fee is hereby granted, provided that this entire notice
+ * is included in all copies of any software which is or includes a copy
+ * or modification of this software and in all copies of the supporting
+ * documentation for such software.
+ * THIS SOFTWARE IS BEING PROVIDED "AS IS", WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTY.  IN PARTICULAR, NEITHER THE AUTHORS NOR AT&T MAKE ANY
+ * REPRESENTATION OR WARRANTY OF ANY KIND CONCERNING THE MERCHANTABILITY
+ * OF THIS SOFTWARE OR ITS FITNESS FOR ANY PARTICULAR PURPOSE.
+ */
+
+/* modified by Mika Nystrom <mika@cs.caltech.edu> */
+
+
+#include <assert.h>
+#include <stdio.h>
+#include "defs.h"
+#include "mymalloc.h"
+
+struct Site *readone();
+struct Site *nextone();
+
+
+static int setup=0;
+
+mod3_start()
+{
+  setup=0;
+  nsites = 0;
+  sites = (struct Site *) memmalloc(4000*sizeof *sites);
+}
+
+
+mod3_addsite(p)
+     struct Point p;
+{
+  assert(!setup);
+  sites[nsites].coord = p;
+  sites[nsites].sitenbr = nsites;
+  sites[nsites].refcnt = 0;
+  nsites += 1;
+  if (nsites % 4000 == 0) {
+    struct Site *newsites = (struct Site *) memmalloc(sites,(nsites+4000)*sizeof*sites);
+    bcopy(sites,newsites,nsites*sizeof*sites);
+    sites=newsites;
+  }
+
+}
+
+/* sort sites on y, then x, coord */
+int scomp(s1,s2)
+struct Point *s1,*s2;
+{
+	if(s1 -> y < s2 -> y) return(-1);
+	if(s1 -> y > s2 -> y) return(1);
+	if(s1 -> x < s2 -> x) return(-1);
+	if(s1 -> x > s2 -> x) return(1);
+	return(0);
+}
+
+mod3_setup() 
+{
+int i;
+  setup=1;
+qsort(sites, nsites, sizeof *sites, scomp);
+xmin=sites[0].coord.x; 
+xmax=sites[0].coord.x;
+for(i=1; i<nsites; i+=1)
+{	if(sites[i].coord.x < xmin) xmin = sites[i].coord.x;
+	if(sites[i].coord.x > xmax) xmax = sites[i].coord.x;
+};
+ymin = sites[0].coord.y;
+ymax = sites[nsites-1].coord.y;
+}
+
+mod3_delaunay()
+{	
+int c;
+ int i;
+struct Site *(*next)();
+
+ assert(setup); 
+triangulate = 1; 
+
+ next = nextone;
+
+siteidx = 0;
+geominit();
+
+ clear_triples();
+voronoi(triangulate, next); 
+
+}
+
+clear_triples()
+{
+  struct Triple *t=triples;
+  triples=NULL;
+
+  while (t!=NULL) {
+    struct Triple *u=t;
+    t=t->next;
+  }
+
+}
+
+int mod3_gettriple(struct TripleArg *t)
+{
+  if (!triples) 
+    return 0;
+  
+  {
+    struct Triple *u=triples;
+    *t = triples->d;
+
+    triples=u->next;
+
+  }
+}
+
+mod3_voronoi()
+{	
+int c;
+struct Site *(*next)();
+
+ assert(setup); 
+triangulate = 0; 
+
+ next = nextone;
+
+siteidx = 0;
+geominit();
+
+voronoi(triangulate, next); 
+
+}
+
+
+/* return a single in-storage site */
+struct Site *nextone()
+{
+struct Site *s;
+if(siteidx < nsites)
+{	s = &sites[siteidx];
+	siteidx += 1;
+	return(s);
+}
+else	return( (struct Site *)NULL);
+}
+
+
+struct memdata {
+  char *ptr;
+  struct memdata *next;
+};
+
+struct memdata *mem=NULL;
+
+char *memmalloc(siz)
+     int siz;
+{
+  char *res=mymalloc(siz);
+  struct memdata *n=mymalloc(sizeof(struct memdata));
+
+  n->next=mem;
+  n->ptr=res;
+  mem=n;
+
+  return res;
+}
+
+/* clear it all out */
+mod3_finish()
+{
+  struct memdata *q;
+  
+  
+  while (mem) {
+    myfree(mem->ptr);
+    q=mem;
+    mem=mem->next;
+    myfree(q);
+  }
+}
