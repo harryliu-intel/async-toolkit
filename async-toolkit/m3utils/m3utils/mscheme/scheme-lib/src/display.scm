58e6664b458492cebfe35a1b54e875d2ca353483
Mika Nystrom <mika@alum.mit.edu>
2009-03-05 18:08:11 +0000

    gcmonitor mods

--- async-toolkit/m3utils/m3utils/mscheme/scheme-lib/src/display.scm
+++ async-toolkit/m3utils/m3utils/mscheme/scheme-lib/src/display.scm
@@ -7,5 +7,10 @@
 (define dnl #\newline)
 
 (define (dis . x)
-	(display (accumulate string-append "" (all-except-last x)) (last x)))
+	(let ((maybe-port (last x))
+				(everything-else (accumulate string-append "" (all-except-last x))))
+		(if (or (null? maybe-port) 
+						(output-port? maybe-port))
+				(display everything-else maybe-port)
+				(display (string-append everything-else maybe-port)))))
 
