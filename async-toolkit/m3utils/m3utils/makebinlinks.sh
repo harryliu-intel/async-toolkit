fd8472a224a8716a6254c1cbe608da734fba9958
Mika Nystrom <mika@alum.mit.edu>
2009-07-09 10:57:17 +0000

    new script for linking all "important" binaries to ~/bin.`uname -m`

---
+++ async-toolkit/m3utils/m3utils/makebinlinks.sh
@@ -0,0 +1,49 @@
+#!/bin/sh -x
+
+# link binaries to proper spot, run from root of dev tree
+
+ROOT=`pwd`
+BINDIR=`./m3arch.sh`
+TGTDIR="${HOME}/bin.`uname -m`"
+
+PROGS="
+${ROOT}/calarm/anova/program/${BINDIR}/anova
+${ROOT}/calarm/regress/calcmetric/${BINDIR}/calcmetric
+${ROOT}/calarm/htmltable/cgidb/${BINDIR}/cgidb
+${ROOT}/calarm/cumulativeave/${BINDIR}/cumulativeave
+${ROOT}/calarm/anova/curves/${BINDIR}/curves
+${ROOT}/calarm/regress/dcheck/${BINDIR}/dcheck
+${ROOT}/deriv/src/../${BINDIR}/deriv
+${ROOT}/calarm/findate/${BINDIR}/findate
+${ROOT}/calarm/regress/fusedata/${BINDIR}/fusedata
+${ROOT}/calarm/gcoms/gcomsdataextractor/${BINDIR}/gcomsdataextractor
+${ROOT}/calarm/traderulelib/getmktdata/${BINDIR}/getmktdata
+${ROOT}/calarm/getquotes/${BINDIR}/getticker
+${ROOT}/calarm/grabtable/${BINDIR}/grabtable
+${ROOT}/calarm/gridctl/${BINDIR}/gridctl
+${ROOT}/calarm/regress/hcdriver/${BINDIR}/hcdriver
+${ROOT}/calarm/histogram/${BINDIR}/histogram
+${ROOT}/calarm/keypaste/${BINDIR}/keypaste
+${ROOT}/calarm/mktsim/tradeisolator/${BINDIR}/mktisolator
+${ROOT}/mscheme/interactive_r/${BINDIR}/mscheme
+${ROOT}/calarm/picktickers/${BINDIR}/picktickers
+${ROOT}/calarm/polyfit/${BINDIR}/polyfit
+/usr/local/pgsql/bin/psql
+${ROOT}/calarm/regress/qmctl/${BINDIR}/qmctl
+${ROOT}/calarm/m3readline/c/${BINDIR}/readlinefe
+${ROOT}/calarm/regress/${BINDIR}/regress
+${ROOT}/calarm/regress/resample/${BINDIR}/resample
+${ROOT}/calarm/twslib/switchtickers/${BINDIR}/switchtickers
+${ROOT}/calarm/anova/tabulate/${BINDIR}/tabulate
+${ROOT}/throttle/${BINDIR}/throttle
+${ROOT}/calarm/trade/${BINDIR}/trade
+${ROOT}/calarm/twslib/testtrade2/client/${BINDIR}/tradeclient
+${ROOT}/calarm/twslib/testtrade2/server/${BINDIR}/tradeserver
+${ROOT}/calarm/trailingave/${BINDIR}/trailingave
+"
+
+mkdir ${TGTDIR} || echo Proceeding...
+
+for p in ${PROGS}; do
+	ln -sf $p ${TGTDIR}
+done
