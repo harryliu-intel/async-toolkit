07be4cf2c6fe4c3f4d5b06ac8c5e560ba2fca66c
Tom Hutchinson <thomashutchinson@hotmail.com>
2009-11-17 07:04:27 +0000

    weird R updates, stuff from bad CVS hygiene
    regressrunner initial live setup
    
    [mika]

--- async-toolkit/m3utils/m3utils/mscheme/scheme-lib/src/m3.scm
+++ async-toolkit/m3utils/m3utils/mscheme/scheme-lib/src/m3.scm
@@ -15,4 +15,23 @@
 
 (install-procedures-without-exception-handlers)
 
+;; basic support for wrapping objects, only to do methods
+(define (obj-method-wrap obj type)
+   (lambda args
+      (modula-type-op type 'call-method obj (car args) (cdr args))))
+
+;; does this type have any type ops registered?
+(define (have-type-ops? tc) (not (null?  (list-modula-type-ops tc))))
+
+(define (closest-opped-supertype tc)
+   (cond ((have-type-ops? tc) tc)
+         ((> tc (rttype-maxtypecode)) #f)
+         (else (closest-opped-supertype (rttype-supertype
+ tc)))))
+
+(define (closest-type-op a . op)
+   (let ((ctc (closest-opped-supertype (rttype-typecode a))))
+        (apply modula-type-op (append (list ctc (car op) a) (cdr op)))))
+
+
 
