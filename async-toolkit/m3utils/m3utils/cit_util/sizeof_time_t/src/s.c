1067302a376163639ce1df7f531e9f9703513381
Mika Nystrom <mika@alum.mit.edu>
2011-01-01 17:44:44 +0000

    Bug 114 sizeof time_t is...?

---
+++ async-toolkit/m3utils/m3utils/cit_util/sizeof_time_t/src/s.c
@@ -0,0 +1,10 @@
+#include <time.h>
+#include <stdio.h>
+
+int 
+main() 
+{
+  printf("sizeof int    = %d\n", sizeof(int));
+  printf("sizeof long   = %d\n", sizeof(long));
+  printf("sizeof time_t = %d\n", sizeof(time_t));
+}
