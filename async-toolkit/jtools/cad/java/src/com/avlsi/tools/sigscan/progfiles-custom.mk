6b6cd3cd30996be7e918cccf3b0825ce51fa88f6
Liu, Ruokun <harry.liu@intel.com>
2015-03-04 10:22:13 -0800

    Added header and shared library files from Incisive 14.10.002, to avoid build problems because not all systems have it installed.
    [git-p4: depot-paths = "//mrl/sw/intel/": change = 311748]

--- async-toolkit/jtools/cad/java/src/com/avlsi/tools/sigscan/progfiles-custom.mk
+++ async-toolkit/jtools/cad/java/src/com/avlsi/tools/sigscan/progfiles-custom.mk
@@ -7,9 +7,9 @@
 PROGFILES_LINKER := $(GCC)
 PROGFILES_PROJECT_LIB_DIRS := /usr/lib
 ifeq ($(MAKEFILE_MACHINE_TYPE),x86_64)
-    PROGFILES_PROJECT_LIB_DIRS += /nfs/site/eda/tools/cadence/incisive/14.10.002/linux_2.6.16_x86-64/tools/lib/64bit
+    PROGFILES_PROJECT_LIB_DIRS += $(CURR_PROJECT_DIR)/transrecord/lib64
 else
-    PROGFILES_PROJECT_LIB_DIRS += /nfs/site/eda/tools/cadence/incisive/14.10.002/linux_2.6.16_x86-64/tools/lib
+    PROGFILES_PROJECT_LIB_DIRS += $(CURR_PROJECT_DIR)/transrecord/lib
 endif
 PROGFILES_PROJECT_LIBS := stdc++ dwapi dcapi sdi2-sst2-gcc_4.1
 PROGFILES_PROJECT_LINK_FLAGS := -shared
