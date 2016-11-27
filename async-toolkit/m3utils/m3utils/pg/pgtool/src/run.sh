83fbcae0361795cb5b90c754b0e9082fabe0da41
Mika Nystrom <mika.nystroem@intel.com>
2016-11-27 23:01:56 +0000

    added -copyrightpath
    Copied from Perforce
     Change: 338267

--- async-toolkit/m3utils/m3utils/pg/pgtool/src/run.sh
+++ async-toolkit/m3utils/m3utils/pg/pgtool/src/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-time ../AMD64_LINUX/genpg -skipholes -bits 28 -sv full.sv /nfs/sc/disks/hlp_0100/yuqisong/hlp-pg-rtl/hlp-work/src/srdl/policy_group.csv 
+time ../AMD64_LINUX/genpg -skipholes -bits 28 -sv full.sv -copyrightpath ${MODEL_ROOT}/scripts/intelcopyright.txt /nfs/sc/disks/hlp_0100/yuqisong/hlp-pg-rtl/hlp-work/src/srdl/policy_group.csv 
