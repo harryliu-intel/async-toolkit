5761f7dc4089dd3e4775ab09da13017dc9481063
Mika Nystrom <mika.nystroem@intel.com>
2017-08-23 23:39:37 +0000

    files in the wrong place
    Copied from Perforce
     Change: 344254

--- async-toolkit/m3utils/m3utils/mscheme/scheme-lib/display.scm
+++
@@ -1,9 +0,0 @@
-;;
-;; $Id: display.scm,v 1.1 2008/10/23 23:37:45 mika Exp $
-;;
-
-(define dnl #\newline)
-
-(define (dis . x)
-	(display (accumulate string-append "" (all-except-last x)) (last x)))
-
