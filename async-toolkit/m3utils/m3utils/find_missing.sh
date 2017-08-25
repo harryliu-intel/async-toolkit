37a17f6fb26938990803440e258eac5ba4f96e97
Mika Nystrom <mika.nystroem@intel.com>
2017-08-25 16:03:17 +0000

    documentation only
    Copied from Perforce
     Change: 344326

--- async-toolkit/m3utils/m3utils/find_missing.sh
+++ async-toolkit/m3utils/m3utils/find_missing.sh
@@ -1,2 +1,5 @@
 #!/bin/sh -x
+
+# make sure you run make_filelist.sh before running this script
+
 cat intel-p4.filelist intel-p4.filelist intel-find.filelist | grep -v AMD64 | grep -v bdd.old | grep -v ARM_LINUX | grep -v /old/ | grep -v '~$' | grep -v /gnuplots/ | grep -v /00 | grep -v 'bak\.../' | grep -v scc0 | grep -v /CVS/ | grep -v ecc/output | grep -v ecc/csrc | grep -v simv.daidir | grep -v sc_ion | sort | uniq -c | sort -g | grep -v /bak0 | grep -v vplot/src/xa | grep -v inter.vpd | grep -v 'fsdb$' | grep " *1 " | less
