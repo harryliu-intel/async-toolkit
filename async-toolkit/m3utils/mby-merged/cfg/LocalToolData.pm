fdd2d8278c7864e97449e73ac2c6fc6048a4d074
Michael Wrighton <michael.wrighton@intel.com>
2018-03-28 08:46:47 -0700

    update to use hdk version of natural docs, local developed 'big' stylesheet

--- async-toolkit/m3utils/mby-merged/cfg/LocalToolData.pm
+++ async-toolkit/m3utils/mby-merged/cfg/LocalToolData.pm
@@ -212,9 +212,10 @@ $ToolConfig_tools{"mgm"} = {
 };
 
 $ToolConfig_tools{"NaturalDocs"} = {
-    VERSION => "atmhdk/1.51.bcs1",
+    VERSION => "1.52",
     # PATH => "$ENV{RTL_PROJ_TOOLS}/natural_docs/&get_tool_version()",
-    PATH => "/nfs/sc/disks/mst_104/mwrighto/NaturalDocs/1.52",
-    EXEC => "&get_tool_path()/NaturalDocs",
+    PATH => "$ENV{RTL_CAD_ROOT}/natural_docs/NaturalDocs/&get_tool_version()",
+    # NaturalDocs really more of a Windows program, doesn't have 'execute' permission set of perl flag
+    EXEC => "perl &get_tool_path()/NaturalDocs",
 };
 
