c2f2914dcfba9e00576d38338cd063da3c1f9f7f
Michael Wrighton <michael.wrighton@intel.com>
2018-04-18 15:25:34 -0700

    towards cm3 white model build from mby RDL automation (cm3 compile error seen on generated code)

--- async-toolkit/m3utils/mby-merged/wm/src/main/m3/genviews/src/builddefs.sh
+++ async-toolkit/m3utils/mby-merged/wm/src/main/m3/genviews/src/builddefs.sh
@@ -1,10 +1,8 @@
-files=${WD}/hlp_top_map.rdl
-
-files=${WD}/hlp_top_map.rdl
+files=${WD}/mby_top_map.rdl
 #files=${WD}/hlp_parser.rdl
 #files=${WD}/hlp_msec.rdl
 
-top_map=hlp_top_map
+top_map=mby_top_map
 
 whichtest=speedtest
 #whichtest=structassign
