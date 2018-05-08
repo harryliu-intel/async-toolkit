1294dad69af59f4fb70c738faba44d519cfdf635
Michael Wrighton <michael.wrighton@intel.com>
2018-05-08 14:53:34 -0700

    update white model makefiles to align with Mika's work on > 1 struct/enum output, model server

--- async-toolkit/m3utils/mby-merged/cfg/LocalToolData.pm
+++ async-toolkit/m3utils/mby-merged/cfg/LocalToolData.pm
@@ -86,7 +86,7 @@ $ToolConfig_tools{dc_shell} = {
 
 $ToolConfig_tools{meta} = {
     PATH => "$ENV{RTL_PROJ_TOOLS}/meta/nhdk/&get_tool_version()",
-    VERSION => "18.04.24",
+    VERSION => "18.05.08",
 };
 
 $ToolConfig_tools{cm3} = {
