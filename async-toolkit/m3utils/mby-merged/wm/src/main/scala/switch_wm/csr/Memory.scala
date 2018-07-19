7456fb60f19e5abf080386d549137c0b284e7553
Kotwica, Michalina <michalina.kotwica@intel.com>
2018-07-19 12:32:42 -0700

    Scala: add AddressRange

--- async-toolkit/m3utils/mby-merged/wm/src/main/scala/switch_wm/csr/Memory.scala
+++ async-toolkit/m3utils/mby-merged/wm/src/main/scala/switch_wm/csr/Memory.scala
@@ -216,4 +216,50 @@ object Memory {
     /** From raw memory */
     def apply[M <: MemoryUnit](munit: M): Address = Address(munit.toBits)
   }
+
+
+  /** Contiguous range of addresses.
+    *
+    * @param pos starting poing of the range
+    * @param width width in bits
+    * */
+  class AddressRange(val pos: Address, val width: Bits) {
+    /** First address free after this range,
+      * e.g. AddressRange(Address(32.bytes), 8.bytes).lim == Address(40.bytes)
+      */
+    def lim: Address = pos + width
+
+    /** Position and the next position after the range,
+     * e.g. AddressRange(Address(32.bytes), 8.bytes).toAddressPair == (Address(32.bytes), Address(40.bytes)) */
+    def toAddressPair = (pos, lim)
+  }
+  object AddressRange {
+    /** Simple explcit constructor */
+    def apply[M <: MemoryUnit](addr: Address, munit: M) = new AddressRange(addr, munit.toBits)
+
+    /** Field constructor */
+    def makeField[M <: MemoryUnit](addr: Address, munit: M) = AddressRange(addr, munit.toBits)
+
+    /** Register constructor.
+      *
+      * Arguments' meaning and relation to addressing --- @see Addressing
+      */
+    def placeReg(at: Address,
+                 regwidth: Alignment,
+                 alignment: Option[Alignment] = None,
+                 accesswidth: Option[Alignment] = None,
+                 addressing: Addressing.Value = Addressing.default): AddressRange = {
+      // 1. addressing is compact => use accesswidth (otherwise use regwidth)
+      // 2. default accesswidth == regwidth
+      // 3. if explicit alignment is specified, use it instead
+      // 4. accesswidth must be no bigger than regwidth
+      accesswidth.foreach(x => require(x <= regwidth))
+      val width = accesswidth
+        .filter(_ => addressing == Addressing.Compact)
+        .getOrElse(regwidth)
+      val align = alignment.getOrElse(width)
+      val lo = at.modAlign(align)
+      AddressRange(lo, (lo + regwidth.toBytes).toBits)
+    }
+  }
 }
