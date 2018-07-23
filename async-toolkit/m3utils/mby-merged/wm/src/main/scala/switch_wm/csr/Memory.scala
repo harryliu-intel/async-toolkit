11b5b6e3a27bb0c1ec53f27bc884f5014c0d58ce
Kotwica, Michalina <michalina.kotwica@intel.com>
2018-07-23 06:52:37 -0700

    Scala: memory implicit conversions

--- async-toolkit/m3utils/mby-merged/wm/src/main/scala/switch_wm/csr/Memory.scala
+++ async-toolkit/m3utils/mby-merged/wm/src/main/scala/switch_wm/csr/Memory.scala
@@ -262,4 +262,21 @@ object Memory {
       AddressRange(lo, (lo + regwidth.toBytes).toBits)
     }
   }
+
+
+  /** Some handy conversions */
+  object ImplicitConversions {
+    /** Conversion to option for memory units. Works with indirect conversions too. */
+    implicit def muToOption[A, M <: MemoryUnit](value: A)(implicit f: A => M) = Some(f(value))
+
+    /** Conversion to option for alignment. Works with indirect conversions too. */
+    implicit def alToOption[A](value: A)(implicit f: A => Alignment) = Some(f(value))
+
+    /** Conversion to alignment for full bytes memory units. Works with indirect conversions too. */
+    implicit def muToAl[A, M <: FullBytes](value: A)(implicit f: A => M): Alignment = f(value).toAlignment
+
+    /** Conversion to addressing from option. Works with indirect conversions too. */
+    implicit def optionToAddressing[A](value: A)(implicit f: A => Option[Addressing.Value]): Addressing.Value =
+      f(value).getOrElse(Addressing.default)
+  }
 }
