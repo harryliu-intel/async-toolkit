9bf28863230e0c5b7a91929c340fdae7abc9f964
Mika Nystrom <mika.nystroem@intel.com>
2018-07-28 08:43:50 -0700

    fix SendPacket prototype and implement dummy SendPacket

---
+++ async-toolkit/m3utils/mby-merged/wm/src/main/c/mby_model_basic.c
@@ -0,0 +1,15 @@
+#include "../m3/genviews/src/build_c/mby_c/src/mby_top_map.h"
+#include "../m3/genviews/src/build_c/mby_c/src/mby_top_map_main.h"
+#include "../m3/model_server/src/model_c_write.h" // pull in write_field
+
+// we implement the interface required of us by the model_server
+
+void
+mby_top_map_SendPacket(const mby_top_map       *r,
+                       const mby_top_map__addr *w,
+                       int                      port,
+                       unsigned char           *packet,
+                       unsigned int             length)
+{
+
+}
