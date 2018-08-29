3501ec7ee50cdbc449e3abb49547d54180bbc419
Grandi, Andrea <andrea.grandi@intel.com>
2018-08-29 16:53:44 -0700

    Refactor Pipeline() function
    The function can actually be removed if it is no longer used.
    
    Change-Id: I5c31d071f00389feaf7891588a929e637033c363

--- async-toolkit/m3utils/mby-merged/src/mby_pipeline.c
+++ async-toolkit/m3utils/mby-merged/src/mby_pipeline.c
@@ -61,6 +61,7 @@ void TxPipeline
     TxStats    (regs, &mod2txs,  txs2mac);
 }
 
+// TODO remove if no longer required
 void Pipeline
 (
     fm_uint32                       regs[MBY_REGISTER_ARRAY_SIZE],
@@ -68,46 +69,8 @@ void Pipeline
           mbyTxStatsToTxMac * const txs2mac
 )
 {
-    // Intermediate structs:
-    mbyParserToMapper     par2map;
-    mbyMapperToClassifier map2cla;
-    mbyClassifierToHash   cla2hsh;
-    mbyHashToNextHop      hsh2nxt;
-    mbyNextHopToMaskGen   nxt2msk;
-    mbyMaskGenToTriggers  msk2trg;
-    mbyTriggersToCongMgmt trg2cgm;
-    mbyCongMgmtToRxStats  cgm2rxs;
     mbyRxStatsToRxOut     rxs2rxo;
-    mbyTxInToModifier     txi2mod;
-    mbyModifierToTxStats  mod2txs;
 
-    // RX pipeline stages:
-
-    Parser     (regs,  mac2par, &par2map);
-
-    Mapper     (regs, &par2map, &map2cla);
-
-    Classifier (regs, &map2cla, &cla2hsh);
-
-    Hash       (regs, &cla2hsh, &hsh2nxt);
-
-    NextHop    (regs, &hsh2nxt, &nxt2msk);
-
-    MaskGen    (regs, &nxt2msk, &msk2trg);
-
-    Triggers   (regs, &msk2trg, &trg2cgm);
-
-    CongMgmt   (regs, &trg2cgm, &cgm2rxs);
-
-    RxStats    (regs, &cgm2rxs, &rxs2rxo);
-
-    // RX to TX adapter:
-
-    RxToTx     (regs, &rxs2rxo, &txi2mod);
-
-    // TX pipeline stages:
-    
-    Modifier   (regs, &txi2mod, &mod2txs);
-
-    TxStats    (regs, &mod2txs,  txs2mac);
+    RxPipeline (regs,  mac2par, &rxs2rxo);
+    TxPipeline (regs, &rxs2rxo, txs2mac);
 }
