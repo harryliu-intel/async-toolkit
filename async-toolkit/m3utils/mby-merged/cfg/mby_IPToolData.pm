2f831a6b994764e100c5e445038ba4e749af3186
Lee, Kok Lim Patrick <kok.lim.patrick.lee@intel.com>
2018-10-01 16:03:40 -0700

    renamed epl -> epc

--- async-toolkit/m3utils/mby-merged/cfg/mby_IPToolData.pm
+++ async-toolkit/m3utils/mby-merged/cfg/mby_IPToolData.pm
@@ -56,7 +56,7 @@ IPToolDataExtras::import_files("mby", \%ToolConfig_ips);
 
 my $epl_version = "eth_port-dev-x0-18ww39c";
 
-$ToolConfig_ips{epl} = {
+$ToolConfig_ips{epc} = {
    PATH    => "$ENV{IP_MODELS}/eth_port/$epl_version",
    VERSION => "$epl_version",
    OTHER   => {
@@ -67,6 +67,6 @@ $ToolConfig_ips{epl} = {
 ######################################################################
 # Executes the import statement above
 ######################################################################
-IPToolDataExtras::import_files("epl", \%ToolConfig_ips);
+IPToolDataExtras::import_files("epc", \%ToolConfig_ips);
 
 1;
