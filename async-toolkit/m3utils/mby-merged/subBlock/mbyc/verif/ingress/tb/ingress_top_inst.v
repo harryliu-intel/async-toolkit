c2901a5279a603d51740009cbc4a4540c559cedb
Godinez Carrillo, Jose J <jose.j.godinez.carrillo@intel.com>
2018-10-03 15:15:45 -0700

    Updating igr testbench:  - Adding igr_rtl_lib to the igr testbench model.  - Fixing igr_rtl_lib to include missing files/intf/incdirs.  - Fixing a couple of typos in igr_top/epl_shim (with Ed's blessing).  - Adding the igr_top instance to the igr_tb top file.
    Signed-off-by: Godinez Carrillo, Jose J <jose.j.godinez.carrillo@intel.com>

--- async-toolkit/m3utils/mby-merged/subBlock/mbyc/verif/ingress/tb/ingress_top_inst.v
+++ async-toolkit/m3utils/mby-merged/subBlock/mbyc/verif/ingress/tb/ingress_top_inst.v
@@ -18,7 +18,57 @@
 // 21.08.2018 : created
 //-----------------------------------------------------------------------------
 
-//igr_top ingress(
-//                .clk   (ingress_primary_clock)
-//               ,.rst_n (ingress_primary_reset)
-//               );
+rx_ppe_igr_if rx_ppe_igr_if_i();
+igr_rx_ppe_if igr_rx_ppe_if_i();
+
+always_comb rx_ppe_igr_if_i.intf0 = 0;
+always_comb rx_ppe_igr_if_i.intf1 = 0;
+
+igr_top igr_top_i (
+  .cclk(ingress_primary_clock),
+	.rst(ingress_primary_reset),
+	.grp_A_rx_ecc(eth_bfm_tx_intf_0.ecc),
+	.grp_B_rx_ecc(eth_bfm_tx_intf_1.ecc),
+	.grp_C_rx_ecc(eth_bfm_tx_intf_2.ecc),
+	.grp_D_rx_ecc(eth_bfm_tx_intf_3.ecc),
+	.grp_A_rx_port_num(eth_bfm_tx_intf_0.port_num),
+	.grp_B_rx_port_num(eth_bfm_tx_intf_1.port_num),
+	.grp_C_rx_port_num(eth_bfm_tx_intf_2.port_num),
+	.grp_D_rx_port_num(eth_bfm_tx_intf_3.port_num),
+	.grp_A_rx_data_valid(eth_bfm_tx_intf_0.data_valid),
+	.grp_B_rx_data_valid(eth_bfm_tx_intf_1.data_valid),
+	.grp_C_rx_data_valid(eth_bfm_tx_intf_2.data_valid),
+	.grp_D_rx_data_valid(eth_bfm_tx_intf_3.data_valid),
+	.grp_A_rx_metadata(eth_bfm_tx_intf_0.metadata),
+	.grp_B_rx_metadata(eth_bfm_tx_intf_1.metadata),
+	.grp_C_rx_metadata(eth_bfm_tx_intf_2.metadata),
+	.grp_D_rx_metadata(eth_bfm_tx_intf_3.metadata),
+	.grp_A_rx_time_stamp(36'b0),
+	.grp_B_rx_time_stamp(36'b0),
+	.grp_C_rx_time_stamp(36'b0),
+	.grp_D_rx_time_stamp(36'b0),
+	.grp_A_rx_data_w_ecc(eth_bfm_tx_intf_0.data_w_ecc),
+	.grp_B_rx_data_w_ecc(eth_bfm_tx_intf_1.data_w_ecc),
+	.grp_C_rx_data_w_ecc(eth_bfm_tx_intf_2.data_w_ecc),
+	.grp_D_rx_data_w_ecc(eth_bfm_tx_intf_3.data_w_ecc),
+	.grp_A_rx_pfc_xoff(eth_bfm_tx_intf_0.pfc_xoff),
+	.grp_B_rx_pfc_xoff(eth_bfm_tx_intf_1.pfc_xoff),
+	.grp_C_rx_pfc_xoff(eth_bfm_tx_intf_2.pfc_xoff),
+	.grp_D_rx_pfc_xoff(eth_bfm_tx_intf_3.pfc_xoff),
+	.grp_A_rx_flow_control_tc(eth_bfm_tx_intf_0.flow_control_tc),
+	.grp_B_rx_flow_control_tc(eth_bfm_tx_intf_1.flow_control_tc),
+	.grp_C_rx_flow_control_tc(eth_bfm_tx_intf_2.flow_control_tc),
+	.grp_D_rx_flow_control_tc(eth_bfm_tx_intf_3.flow_control_tc),
+	.vp_rx_ecc(eth_bfm_tx_intf_4.ecc),
+	.vp_rx_port_num(eth_bfm_tx_intf_0.port_num),
+	.vp_rx_data_valid(eth_bfm_tx_intf_0.data_valid),
+	.vp_rx_metadata(eth_bfm_tx_intf_0.metadata),
+	.vp_rx_time_stamp(36'b0),
+	.vp_cpp_rx_metadata(20'b0),
+	.vp_rx_data_w_ecc(eth_bfm_tx_intf_0.data_w_ecc),
+	.vp_rx_flow_control_tc(eth_bfm_tx_intf_0.flow_control_tc),
+	.vp_tx_pfc_xoff(eth_bfm_tx_intf_0.pfc_xoff),
+	.rx_ppe_igr(rx_ppe_igr_if_i.igr),
+	.igr_rx_ppe(igr_rx_ppe_if_i.igr)
+  
+);
