1b1949125d36063cbcf6e88f1d3b589e625ee834
Nanal, Subodh S <subodh.s.nanal@intel.com>
2018-10-05 11:37:09 -0700

    FC tb cleanup

--- async-toolkit/m3utils/mby-merged/verif/tb/env/fc_env_pkg.sv
+++ async-toolkit/m3utils/mby-merged/verif/tb/env/fc_env_pkg.sv
@@ -27,14 +27,9 @@ package fc_env_pkg;
     import svt_uvm_pkg::*;
     import svt_bfm_shell_uvm_pkg::*;
     import svt_amba_uvm_pkg::*;
-    //import svt_axi_uvm_pkg::*;
-    //import svt_apb_uvm_pkg::*;
 
     // VTE UVM ip1 pkg (native UVM based)
     import ip1_uvm_pkg::*;
-
-    //// VTE UVM ip2 pkg (SLU based)
-    //import ip2_sla_pkg::*;
 
     // VTE UVM ip3 pkg (native UVM based)
     import ip3_uvm_env_pkg::*;
@@ -70,34 +65,6 @@ package fc_env_pkg;
     `include "fc_ral_env.svh"
 
     `include "subsystem_base_env.svh"
-
-    `ifdef AXI_ENV_ENABLE
-    // AXI BFM
-//    `include "axi_bfm_defines.svh"
-//    `include "axi_master_txn.svh"
-//    `include "axi_system_cfg.svh"
-//    `include "axi_scoreboard.svh"
-//    `include "svt_axi_virtual_sequencer.svh"
-//    `include "axi_bfm_env.svh"
-//    `include "axi_integ_env.svh"
-    `endif
-
-    // APB BFM
-    `ifdef APB_ENV_ENABLE
-//    `include "apb_bfm_defines.svh"
-//    `include "apb_master_txn.svh"
-//    `include "apb_shared_cfg.svh"
-//    `include "apb_bfm_env.svh"
-//    `include "apb_integ_env.svh"
-    `endif
-
-    // CHI BFM
-    `ifdef CHI_ENV_ENABLE
-    //import svt_chi_uvm_pkg::*;
-    `include "chi_system_cfg.svh"
-    `include "chi_bfm_env.svh"
-    `include "chi_integ_env.svh"
-    `endif
 
     // VTE-UVM-TR
     `ifdef VTE_IP3_UVM_ENV_ENABLE
