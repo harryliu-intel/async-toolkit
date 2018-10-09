3fd00908db9c8401d84e080ac1f2105c53fd7529
Grandi, Andrea <andrea.grandi@intel.com>
2018-10-09 16:05:35 -0700

    Remove #ifdef in classifier since it compiles OK
    Change-Id: I15e81b0f39bac49f2db9b2ea325a1827ff31978a

--- async-toolkit/m3utils/mby-merged/wm/src/main/c_expmt/src/mby_classifier.c
+++ async-toolkit/m3utils/mby-merged/wm/src/main/c_expmt/src/mby_classifier.c
@@ -34,7 +34,6 @@ void doAction
     fm_byte prec   = FM_GET_FIELD        (action, MBY_FFU_ACTION, PREC);
     fm_byte encode = FM_GET_UNNAMED_FIELD(action, MBY_FFU_ACTION_l_ENTRYTYPE, 5);
 
-#ifndef USE_NEW_CSRS // does not compile with new CSRS: missing defines <-- FIXME !!!!
     mbyClassifierActionEntryType entryType =
         (encode == 1)                ? MBY_FFU_ACTION_SET4_4B  :
         (encode == 2)                ? MBY_FFU_ACTION_SET8_1B  :
@@ -127,7 +126,6 @@ void doAction
         case MBY_FFU_ACTION_NOP:
             break;
     }
-#endif
 }
 
 static void applyEntropyKeyMask
