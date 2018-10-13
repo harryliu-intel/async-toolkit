8f4351749b61de21f60c3a28fb059b184db631b1
Khoo, Lay Hock <lay.hock.khoo@intel.com>
2018-10-13 10:10:24 -0700

    Reverted epc tool version

--- async-toolkit/m3utils/mby-merged/cfg/mby_IPToolData.pm
+++ async-toolkit/m3utils/mby-merged/cfg/mby_IPToolData.pm
@@ -65,7 +65,8 @@ $ToolConfig_ips{mby}{ENV}{LD_LIBRARY_PATH} .=  join(':',
 my $epl_version = "eth_port-dev-x0-18ww39c";
 
 $ToolConfig_ips{epc} = {
-   PATH    => "/nfs/sc/disks/sc_mby_00055/layhockk/mby/work_root/MBY/$epl_version",
+   #PATH    => "/nfs/sc/disks/sc_mby_00055/layhockk/mby/work_root/MBY/$epl_version",
+   PATH    => "$ENV{IP_MODELS}/eth_port/$epl_versio",
    VERSION => "$epl_version",
    OTHER   => {
       SEARCH_PATHS => ["&get_tool_path()",],
