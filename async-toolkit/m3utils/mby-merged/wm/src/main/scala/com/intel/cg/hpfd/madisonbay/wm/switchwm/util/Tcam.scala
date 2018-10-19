d39eb0deea35ee6730de089c8b834e0401979110
Dominik Dagiel <dominik.dagiel@intel.com>
2018-10-19 10:15:13 +0200

    Cleanup code
    Change-Id: I4c079e1a92930504de378c34ed6799e0468052f8

--- async-toolkit/m3utils/mby-merged/wm/src/main/scala/com/intel/cg/hpfd/madisonbay/wm/switchwm/util/Tcam.scala
+++ async-toolkit/m3utils/mby-merged/wm/src/main/scala/com/intel/cg/hpfd/madisonbay/wm/switchwm/util/Tcam.scala
@@ -5,11 +5,8 @@ import com.intel.cg.hpfd.madisonbay.wm.utils.extensions.ExtLong.Implicits
 object Tcam {
   case class TcamQuery(keyInvert: Boolean, key: Boolean, input: Boolean)
 
-  type TcamMatchingBehavior = TcamQuery => Boolean
-
-  def standardTcamMatchBit(x: TcamQuery): Boolean = {
+  def tcamMatchBit(x: TcamQuery): Boolean = {
     x match {
-      // keyInvert: Boolean, key: Boolean,  input: Boolean
       case TcamQuery(false, false,  _)    => false // invalid
       // match 0
       case TcamQuery(false, true,  false) => true
@@ -22,7 +19,7 @@ object Tcam {
     }
   }
 
-  def tcamMatchLong(behavior: TcamMatchingBehavior)(bits: Int)(keyInvert: Long, key: Long, input: Long): Boolean = {
+  def tcamMatchLong(behavior: TcamQuery => Boolean)(bits: Int)(keyInvert: Long, key: Long, input: Long): Boolean = {
     (0 until bits).map(pos => TcamQuery(keyInvert.getBit(pos), key.getBit(pos), input.getBit(pos))).forall(behavior)
   }
 }
