a79a633a4e2ec4b5cb970d6fef20e12a090dec59
Paul J. Pladsen <paul.j.pladsen@intel.com>
2018-11-01 07:27:37 -0700

    Switched to using shdv_base_tb_sequencer.

--- async-toolkit/m3utils/mby-merged/subBlock/mbyc/verif/ingress/tests/mby_igr_rand_test/mby_igr_rand_test.svh
+++ async-toolkit/m3utils/mby-merged/subBlock/mbyc/verif/ingress/tests/mby_igr_rand_test/mby_igr_rand_test.svh
@@ -97,7 +97,7 @@ class mby_igr_eth_simple_seq extends mby_igr_extended_base_seq;
 
       `uvm_info(this.get_name(), ("Starting eth simple sequence..."), UVM_LOW)
       foreach(los_sequencers[i]) begin
-         assert($cast(los_sequencers[i], mby_igr_env_pkg::mby_igr_tb_sequencer::pick_sequencer($sformatf("eth_bfm_%0d_rx0", i))))
+         assert($cast(los_sequencers[i], mby_igr_env_pkg::shdv_base_tb_sequencer::pick_sequencer($sformatf("eth_bfm_%0d_rx0", i))))
          else begin
             `uvm_error(get_name(), $sformatf("Could not get a pointer to the sequencer%0d", i));
          end
@@ -161,7 +161,6 @@ class mby_igr_rand_test extends mby_igr_base_test;
    //---------------------------------------------------------------------------
    function void build_phase(uvm_phase phase);
       super.build_phase(phase);
-      // TODO: Create config obj here.
    endfunction : build_phase
 
    //---------------------------------------------------------------------------
