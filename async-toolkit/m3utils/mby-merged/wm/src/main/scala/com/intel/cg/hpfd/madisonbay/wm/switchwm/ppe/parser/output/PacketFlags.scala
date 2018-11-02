010926a52e203f9fb36ebd5ecbda13bdd2a5b0b5
Dominik Dagiel <dominik.dagiel@intel.com>
2018-11-02 14:03:43 +0100

    Replace Enumeration with sealed trait
    Change-Id: I65f0eab7807a172ea6bdc7e7da4a566a6b097534

--- async-toolkit/m3utils/mby-merged/wm/src/main/scala/com/intel/cg/hpfd/madisonbay/wm/switchwm/ppe/parser/output/PacketFlags.scala
+++ async-toolkit/m3utils/mby-merged/wm/src/main/scala/com/intel/cg/hpfd/madisonbay/wm/switchwm/ppe/parser/output/PacketFlags.scala
@@ -29,18 +29,59 @@ object PacketFlags {
 
   def apply(bitSet: BitSet): PacketFlags = new PacketFlags(bitSet)
 
+  sealed trait GenericPacketFlag
+  case object Flag_NOP                extends GenericPacketFlag
+  case object Flag_otr_l2_vlan1       extends GenericPacketFlag
+  case object Flag_otr_l2_vlan2       extends GenericPacketFlag
+  case object Flag_otr_l2_v2first     extends GenericPacketFlag
+  case object Flag_otr_l4_udp_v       extends GenericPacketFlag
+  case object Flag_otr_l4_tcp_v       extends GenericPacketFlag
+  case object Flag_otr_ipv6_v         extends GenericPacketFlag
+  case object Flag_otr_head_frag_v    extends GenericPacketFlag
+  case object Flag_otr_payload_frag_v extends GenericPacketFlag
+  case object Flag_otr_mpls_v         extends GenericPacketFlag
+  case object Flag_otr_l3_v           extends GenericPacketFlag
+  case object Flag_otr_l4_v           extends GenericPacketFlag
+  case object Flag_inr_ipv6_v         extends GenericPacketFlag
+  case object Flag_inr_l2_v           extends GenericPacketFlag
+  case object Flag_inr_l2_vlan1       extends GenericPacketFlag
+  case object Flag_inr_l2_vlan2       extends GenericPacketFlag
+  case object Flag_inr_l2_v2first     extends GenericPacketFlag
+  case object Flag_inr_mpls_v         extends GenericPacketFlag
+  case object Flag_inr_l3_v           extends GenericPacketFlag
+  case object Flag_inr_l4_v           extends GenericPacketFlag
+  case object Flag_GeneralFlags       extends GenericPacketFlag
+
+  private val GenericFlags = Map(
+    0 -> Flag_NOP,
+    1 -> Flag_otr_l2_vlan1,
+    2 -> Flag_otr_l2_vlan2,
+    3 -> Flag_otr_l2_v2first,
+    4 -> Flag_otr_l4_udp_v,
+    5 -> Flag_otr_l4_tcp_v,
+    6 -> Flag_otr_ipv6_v,
+    10 -> Flag_otr_head_frag_v,
+    11 -> Flag_otr_payload_frag_v,
+    21 -> Flag_otr_mpls_v,
+    22 -> Flag_otr_l3_v,
+    23 -> Flag_otr_l4_v,
+    24 -> Flag_inr_ipv6_v,
+    25 -> Flag_inr_l2_v,
+    26 -> Flag_inr_l2_vlan1,
+    27 -> Flag_inr_l2_vlan2,
+    28 -> Flag_inr_l2_v2first,
+    29 -> Flag_inr_mpls_v,
+    30 -> Flag_inr_l3_v,
+    31 -> Flag_inr_l4_v
+  )
+
   //scalastyle:off
-  object TypicalPacketFlags extends Enumeration {
-    val NOP: Value                  = Value(0, "NOP")
-    val otr_l2_vlan1: Value         = Value(1, "otr_l2_vlan1")
-    val otr_l2_vlan2: Value         = Value(2, "otr_l2_vlan2")
-    val otr_l2_v2first: Value       = Value(3, "otr_l2_v2first")
-    val otr_l4_udp_v: Value         = Value(4, "otr_l4_udp_v")
-    val otr_l4_tcp_v: Value         = Value(5, "otr_l4_tcp_v")
-    val otr_ipv6_v: Value           = Value(6, "otr_ipv6_v")
-    val future: IndexedSeq[Value]   = (12 to 20).map(Value(_, "Available for future cases not defined currently"))
-    val generic: IndexedSeq[Value]  = (32 to 47).map(Value(_, "gf"))
+  def getGenericFlag(idFlag: Int): Option[GenericPacketFlag] = idFlag match {
+    case n if GenericFlags.contains(n) => Some(GenericFlags(n))
+    case n if (n >=7 && n <= 9) || (n >= 12 && n <= 20) || (n>=32 && n<=47) => Some(Flag_GeneralFlags)
+    case _ => None
   }
-  //scalastyle:on
+
+  def getGenericFlagId(flag: GenericPacketFlag): Option[Int] = GenericFlags.find { case (_, value) => flag == value }.map(_._1)
 
 }
--- async-toolkit/m3utils/mby-merged/wm/src/test/scala/com/intel/cg/hpfd/madisonbay/wm/switchwm/epl/PacketFlagsSpec.scala
+++ async-toolkit/m3utils/mby-merged/wm/src/test/scala/com/intel/cg/hpfd/madisonbay/wm/switchwm/epl/PacketFlagsSpec.scala
@@ -1,6 +1,7 @@
 package com.intel.cg.hpfd.madisonbay.wm.switchwm.epl
 
 import com.intel.cg.hpfd.madisonbay.wm.switchwm.ppe.parser.output.PacketFlags
+import com.intel.cg.hpfd.madisonbay.wm.switchwm.ppe.parser.output.PacketFlags._
 import com.intel.cg.hpfd.madisonbay.wm.utils.Binary.BinaryInterpolator
 import org.scalatest._
 
@@ -13,19 +14,33 @@ class PacketFlagsSpec extends FlatSpec with Matchers {
     PacketFlags().toLong shouldEqual 0L
   }
 
-  "Packet Flags" should " fit proper flags" in {
+  it should "fit proper flags" in {
     PacketFlags().set(1).set(3).set(7).toLong.toInt shouldEqual b"10001010"
   }
 
-  "Packet Flags" should " fit proper flags with BitSet constructor" in {
+  it should "fit proper flags with BitSet constructor" in {
     PacketFlags(BitSet(4,5,8)).toInt shouldEqual b"100110000"
   }
 
-  "Packet Flags" should " clear flags" in {
+  it should "clear flags" in {
     PacketFlags(BitSet(4,5,8)).clear(8).toInt shouldEqual b"00110000"
   }
 
-  "Packet Flags" should " assign flags" in {
+  it should "assign flags" in {
     PacketFlags(BitSet(4,5,8)).assign(8, false).assign(1, true).toInt shouldEqual b"00110010"
   }
+
+  it should "properly translate flags IDs" in {
+    val pckFlags = PacketFlags(BitSet(4,5,8,31)).get
+    pckFlags.zip(List(Flag_otr_l4_udp_v, Flag_otr_l4_tcp_v, Flag_GeneralFlags, Flag_inr_l4_v)).
+      count { case (flagId, flagObj) => getGenericFlag(flagId).contains(flagObj) } shouldEqual pckFlags.size
+  }
+
+  it should "properly find flags by IDs" in {
+    val pckFlags = PacketFlags(BitSet(4,5,8,31)).get
+    // don't count General Flags
+    pckFlags.zip(List(Flag_otr_l4_udp_v, Flag_otr_l4_tcp_v, Flag_GeneralFlags, Flag_inr_l4_v)).
+      count { case (flagId, flagObj) => getGenericFlagId(flagObj).contains(flagId) } shouldEqual pckFlags.size - 1
+  }
+
 }
