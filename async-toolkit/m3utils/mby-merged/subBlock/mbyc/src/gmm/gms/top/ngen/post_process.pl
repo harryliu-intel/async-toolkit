268b5bd862f2bc5729c781193790cf348b77e9d2
Jarvis, Michael A <michael.a.jarvis@intel.com>
2018-11-28 20:35:18 -0800

    Add missing paths.

--- async-toolkit/m3utils/mby-merged/subBlock/mbyc/src/gmm/gms/top/ngen/post_process.pl
+++ async-toolkit/m3utils/mby-merged/subBlock/mbyc/src/gmm/gms/top/ngen/post_process.pl
@@ -16,7 +16,17 @@ while (<$infile_fh>) {
    }
    #elsif ($_ =~ m/^logic\w*[68:0][1:0] ;/) {
    elsif ($_ =~ m/^logic/) {
-      # Don't print this line   
+      print $outfile_fh "// Tag ring interface (ingress -to- egress/GMM)\n";
+      print $outfile_fh "mby_tag_ring_t           tag_ring_in [MBY_MAX_NUM_MGP_MSB:0][1:0] ;\n";
+      print $outfile_fh "mby_tag_ring_t           tag_ring_out[MBY_MAX_NUM_MGP_MSB:0][1:0] ;\n";
+      print $outfile_fh "\n";
+      print $outfile_fh "// MultiCast tag ring interafce (MCE-to-egress)\n";
+      print $outfile_fh "mby_mc_tag_ring_t        mc_tag_ring_out_left  [3:0] ;\n";
+      print $outfile_fh "mby_mc_tag_ring_t        mc_tag_ring_out_right [3:0] ;\n";
+      print $outfile_fh "\n";
+      print $outfile_fh "// Dequeue (Egress -to- GMM)\n";
+      print $outfile_fh "mby_unicast_deque_t      mby_deque_from_egr [MBY_MAX_NUM_MGP_MSB:0][1:0];\n";
+      print $outfile_fh "\n";
    }
    else {
       print $outfile_fh $_;
