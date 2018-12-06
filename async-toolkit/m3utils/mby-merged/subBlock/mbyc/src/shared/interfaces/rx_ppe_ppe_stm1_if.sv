e1edddf659910a13466adf40940fc6f2eb0247a6
Bagge, Jon <jon.bagge@intel.com>
2018-12-06 12:38:10 -0800

    Fixed typo

--- async-toolkit/m3utils/mby-merged/subBlock/mbyc/src/shared/interfaces/rx_ppe_ppe_stm1_if.sv
+++ async-toolkit/m3utils/mby-merged/subBlock/mbyc/src/shared/interfaces/rx_ppe_ppe_stm1_if.sv
@@ -36,7 +36,7 @@ interface rx_ppe_ppe_stm1_if
 ();
 logic   [1:0]   [3:0]   tbl_ren;        //per port, per chunk read enables
 logic   [1:0]   [16:0]  tbl_raddr;      //per port read address
-logic   [1:0]   [287:0] tbl_em_rvalid;  //EM ports read valid
+logic   [1:0]           tbl_em_rvalid;  //EM ports read valid
 logic   [1:0]   [287:0] tbl_em_rdata;   //EM ports read data (including ECC bits)
 
 modport ppe(
