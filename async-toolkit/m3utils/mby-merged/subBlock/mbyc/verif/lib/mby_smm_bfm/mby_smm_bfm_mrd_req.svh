f4657a2b5d82f0ceec51a32878c1f9d80aa4e8f9
Orozco Rodriguez, Huber E <huber.e.orozco.rodriguez@intel.com>
2018-12-13 15:14:22 -0800

    Signed-off-by: Orozco Rodriguez, Huber E <huber.e.orozco.rodriguez@intel.com>

--- async-toolkit/m3utils/mby-merged/subBlock/mbyc/verif/lib/mby_smm_bfm/mby_smm_bfm_mrd_req.svh
+++ async-toolkit/m3utils/mby-merged/subBlock/mbyc/verif/lib/mby_smm_bfm/mby_smm_bfm_mrd_req.svh
@@ -52,24 +52,24 @@ class mby_smm_bfm_mrd_req
    )
    extends uvm_pkg::uvm_subscriber #(T_req);
 
-   T_req mem_req;
-   T_req mem_rsp;
+//   T_req mem_req;
+//   T_req mem_rsp;
    
    smm_bfm_row_rd_req_agent rd_req_agent_ptr;
 
    smm_bfm_mem_node  mesh_ptr[NUM_MSH_ROWS-1:0][NUM_MSH_COLS-1:0];
    
    
-   logic [13:0]  addr;
-   logic [3:0]   node_row;
-   logic [2:0]   node_col;
-   logic [511:0] wr_data;
-   logic [511:0] rd_data;
-   logic [12:0]  req_id;
-   logic         rreq_valid;
-   logic         rrsp_valid;
-   time          rreq_delay;
-   time          rrsp_delay;
+//   logic [13:0]  addr;
+//   logic [3:0]   node_row;
+//   logic [2:0]   node_col;
+//   logic [511:0] wr_data;
+//   logic [511:0] rd_data;
+//   logic [12:0]  req_id;
+//   logic         rreq_valid;
+//   logic         rrsp_valid;
+//   time          rreq_delay;
+//   time          rrsp_delay;
  
 
 // Registering class with the factory
@@ -119,56 +119,119 @@ class mby_smm_bfm_mrd_req
    // to this method by outside components should be done via the
    // analysis_export.
    function void write(T_req ap_item);
-      mem_req = ap_item;
-      rreq_valid = 1;
+//      mem_req = ap_item;
+//      rreq_valid = 1;
+      fork
+         mrd_req_rsp(ap_item);
+      join_none
    endfunction
 
 
-   virtual protected task automatic mrd_req();    // TODO: Change to task to make it time consuming for delay modeling
-        `uvm_info("mby_smm_bfm_mrd_req()::mrd_req(): Got into...","",     UVM_MEDIUM);
-     forever begin : issue_mrd_req 
-         wait(rreq_valid == 1);
-         req_id   = mem_req.data_pkt.mim_req_id;
-         addr     = mem_req.data_pkt.mim_seg_ptr[13:0];
-         node_row = mem_req.data_pkt.mim_seg_ptr[17:14];
-         node_col = {mem_req.data_pkt.mim_seg_ptr[1:0]^mem_req.data_pkt.mim_wd_sel,mem_req.data_pkt.mim_seg_ptr[18]};
-         rreq_delay = 833*(node_row+node_col);
-         //#(rreq_delay);
-         rd_data = mesh_ptr[node_row][node_col].mrd(addr);
-         `uvm_info("mby_smm_bfm_mrd_req()::mrd_req(): Address=", $sformatf("%014h", addr),     UVM_MEDIUM);
-         `uvm_info("mby_smm_bfm_mrd_req()::mrd_req(): NodeRow=", $sformatf("%04h",  node_row), UVM_MEDIUM);
-         `uvm_info("mby_smm_bfm_mrd_req()::mrd_req(): NodeCol=", $sformatf("%03h",  node_col), UVM_MEDIUM);
-         `uvm_info("mby_smm_bfm_mrd_req()::mrd_req(): rd_data=", $sformatf("%0128h",rd_data),  UVM_MEDIUM);
-         rrsp_valid = 1;
-         #100;
-         rreq_valid = 0;
-         rrsp_valid = 0;
-     end
-   endtask : mrd_req
+//   virtual protected task automatic mrd_req();    // TODO: Change to task to make it time consuming for delay modeling
+//        `uvm_info("mby_smm_bfm_mrd_req()::mrd_req(): Got into...","",     UVM_MEDIUM);
+//     forever begin : issue_mrd_req 
+//         wait(rreq_valid == 1);
+//         req_id   = mem_req.data_pkt.mim_req_id;
+//         addr     = mem_req.data_pkt.mim_seg_ptr[13:0];
+//         node_row = mem_req.data_pkt.mim_seg_ptr[17:14];
+//         node_col = {mem_req.data_pkt.mim_seg_ptr[1:0]^mem_req.data_pkt.mim_wd_sel,mem_req.data_pkt.mim_seg_ptr[18]};
+//         rreq_delay = 833*(node_row+node_col);
+//         //#(rreq_delay);
+//         rd_data = mesh_ptr[node_row][node_col].mrd(addr);
+//         `uvm_info("mby_smm_bfm_mrd_req()::mrd_req(): Address=", $sformatf("%014h", addr),     UVM_MEDIUM);
+//         `uvm_info("mby_smm_bfm_mrd_req()::mrd_req(): NodeRow=", $sformatf("%04h",  node_row), UVM_MEDIUM);
+//         `uvm_info("mby_smm_bfm_mrd_req()::mrd_req(): NodeCol=", $sformatf("%03h",  node_col), UVM_MEDIUM);
+//         `uvm_info("mby_smm_bfm_mrd_req()::mrd_req(): rd_data=", $sformatf("%0128h",rd_data),  UVM_MEDIUM);
+//         rrsp_valid = 1;
+//         #100;
+//         rreq_valid = 0;
+//         rrsp_valid = 0;
+//     end
+//   endtask : mrd_req
+//
+//   
+//   virtual protected task automatic mrd_rsp();
+//      `uvm_info("mby_smm_bfm_mrd_req()::mrd_rsp(): Got into...","",     UVM_MEDIUM);
+//      forever begin : collect_mem_rd
+//         // Wait for a new transaction to appear
+//         wait(rrsp_valid === 1);
+//         
+//         mem_rsp = T_req::type_id::create("mem_rsp", this);
+//         mem_rsp.data_pkt.mim_rrsp_valid = 1;
+//         mem_rsp.data_pkt.mim_rd_data = rd_data;
+//         mem_rsp.data_pkt.mim_rrsp_req_id = req_id;
+//         rrsp_delay = 833*(node_row+node_col+$urandom_range(0, 16));
+//         //#(rrsp_delay);
+//         `uvm_info("mby_smm_bfm_mrd_req()::mrd_rsp(): Delay=",rrsp_delay,     UVM_MEDIUM);
+//         this.rd_req_agent_ptr.driver.vintf.drive_data(mem_rsp.data_pkt, 0, 0);
+//         // Create the seq_item
+//         `uvm_info("mby_smm_bfm_mrd_req()::mrd_rsp(): Creating the transaction for MemRd Response","", UVM_MEDIUM);
+//         #100;
+//         mem_rsp.data_pkt.mim_rrsp_valid = 0;
+//         rrsp_valid = 0;
+//      end
+//   endtask : mrd_rsp
 
-   
-   virtual protected task automatic mrd_rsp();
-      `uvm_info("mby_smm_bfm_mrd_req()::mrd_rsp(): Got into...","",     UVM_MEDIUM);
-      forever begin : collect_mem_rd
-         // Wait for a new transaction to appear
-         wait(rrsp_valid === 1);
-         
-         mem_rsp = T_req::type_id::create("mem_rsp", this);
-         mem_rsp.data_pkt.mim_rrsp_valid = 1;
-         mem_rsp.data_pkt.mim_rd_data = rd_data;
-         mem_rsp.data_pkt.mim_rrsp_req_id = req_id;
-         rrsp_delay = 833*(node_row+node_col+$urandom_range(0, 16));
-         //#(rrsp_delay);
-         `uvm_info("mby_smm_bfm_mrd_req()::mrd_rsp(): Delay=",rrsp_delay,     UVM_MEDIUM);
-         this.rd_req_agent_ptr.driver.vintf.drive_data(mem_rsp.data_pkt, 0, 0);
-         // Create the seq_item
-         `uvm_info("mby_smm_bfm_mrd_req()::mrd_rsp(): Creating the transaction for MemRd Response","", UVM_MEDIUM);
-         #100;
-         mem_rsp.data_pkt.mim_rrsp_valid = 0;
-         rrsp_valid = 0;
-      end
-   endtask : mrd_rsp
-
+   virtual protected task automatic mrd_req_rsp(T_req mem_req);
+      T_req          mem_rsp;
+      
+      logic [12:0]   req_id;
+      logic [13:0]   addr;
+      logic [3:0]    node_row;
+      logic [2:0]    node_col;
+      logic [511:0]  rd_data;
+      
+      time           rreq_delay;
+      time           rrsp_delay;
+      
+      req_id      = mem_req.data_pkt.mim_req_id;
+      addr        = mem_req.data_pkt.mim_seg_ptr[13:0];
+      node_row    = mem_req.data_pkt.mim_seg_ptr[17:14];
+      node_col    = {mem_req.data_pkt.mim_seg_ptr[1:0]^mem_req.data_pkt.mim_wd_sel,mem_req.data_pkt.mim_seg_ptr[18]};
+      
+      `uvm_info(get_type_name(), $sformatf("mby_smm_bfm_mrd_req::mrd_req_rsp() : Received a memory read request. ReqID = 0x%x", req_id),  UVM_MEDIUM);
+      `uvm_info(get_type_name(), $sformatf("mby_smm_bfm_mrd_req::mrd_req_rsp() : Address = 0x%0x. ReqID = 0x%x", addr, req_id),           UVM_MEDIUM);
+      `uvm_info(get_type_name(), $sformatf("mby_smm_bfm_mrd_req::mrd_req_rsp() : NodeRow = 0x%0x. ReqID = 0x%x", node_row, req_id),       UVM_MEDIUM);
+      `uvm_info(get_type_name(), $sformatf("mby_smm_bfm_mrd_req::mrd_req_rsp() : NodeCol = 0x%0x. ReqID = 0x%x", node_col, req_id),       UVM_MEDIUM);
+      
+      // Process the input memory read request
+      
+      rreq_delay  = 833*(node_row+node_col);
+      
+      `uvm_info(get_type_name(), $sformatf("mby_smm_bfm_mrd_req::mrd_req_rsp() : Simulating read request delay of  = %0d. ReqID = 0x%x", rreq_delay, req_id),  UVM_MEDIUM);
+      
+      #(rreq_delay);
+      
+      rd_data = mesh_ptr[node_row][node_col].mrd(addr);
+      
+      `uvm_info(get_type_name(), $sformatf("mby_smm_bfm_mrd_req::mrd_req_rsp() :  RdData  = 0x%0x. ReqID = 0x%x", rd_data, req_id),  UVM_MEDIUM);
+      
+      //#100;
+      
+      // Now generate the memory read response
+      
+      mem_rsp = T_req::type_id::create("mem_rsp", this);
+      mem_rsp.data_pkt.mim_rrsp_valid  = 1;
+      mem_rsp.data_pkt.mim_rd_data     = rd_data;
+      mem_rsp.data_pkt.mim_rrsp_req_id = req_id;
+      
+      rrsp_delay = 833*(node_row+node_col+$urandom_range(0, 16));
+      
+      `uvm_info(get_type_name(), $sformatf("mby_smm_bfm_mrd_req::mrd_req_rsp() : Simulating read response delay of  = %0d. ReqID = 0x%x", rrsp_delay, req_id),  UVM_MEDIUM);
+      
+      #(rrsp_delay);
+      
+      `uvm_info(get_type_name(), $sformatf("mby_smm_bfm_mrd_req::mrd_req_rsp() : Sending response to driver. ReqID = 0x%x", req_id),  UVM_MEDIUM);
+      
+      // Might want to convert this into a fifo specially on highly concurrent scenarios
+      this.rd_req_agent_ptr.driver.vintf.drive_data(mem_rsp.data_pkt, 0, 0);
+      
+      //#100;
+      
+      mem_rsp.data_pkt.mim_rrsp_valid = 0;
+      
+      `uvm_info(get_type_name(), $sformatf("mby_smm_bfm_mrd_req::mrd_req_rsp() : Request done. ReqID = 0x%x", req_id),  UVM_MEDIUM);
+   endtask : mrd_req_rsp
 
    // -------------------------------------------------------------------------
    // TASK: run_phase
@@ -177,14 +240,14 @@ class mby_smm_bfm_mrd_req
    //
    // -------------------------------------------------------------------------
   task run_phase (uvm_phase phase);
-      fork
-         begin : issue_req
-            mrd_req();
-         end
-         begin : issue_rsp
-            mrd_rsp();
-         end
-      join
+//      fork
+//         begin : issue_req
+//            mrd_req();
+//         end
+//         begin : issue_rsp
+//            mrd_rsp();
+//         end
+//      join
    endtask : run_phase
 
 
