f98f539fbc2b95d7ca94a88a17cf8f222835cf8b
Mika Nystrom <mika.nystroem@intel.com>
2018-12-19 18:49:56 -0800

    simple tests

--- async-toolkit/m3utils/mby-merged/wm/src/main/m3/genviews/src/test_api/test_api.c
+++ async-toolkit/m3utils/mby-merged/wm/src/main/m3/genviews/src/test_api/test_api.c
@@ -1,7 +1,58 @@
+#include <malloc.h>
 #include <stdio.h>
+#define __USE_BSD /* random() */
+#include <stdlib.h>
 #include "hohum.h"
 #include "mby_top_map.h"
 #include "seqtype.h"
+
+void
+print_ragged(const seqtype_t *s)
+{
+  int i=0;
+  printf("(");
+  while(s->d[i] !=-1) {
+    printf(" %d", s->d[i]);
+    ++i;
+  }
+  printf(" )");
+}
+
+#define ASPRINTF(x, ...)                      \
+  do {                                       \
+    size_t _sz = 1 + snprintf(NULL, 0, __VA_ARGS__);      \
+    char *_buff = malloc(_sz);                             \
+    sprintf(_buff, __VA_ARGS__);                         \
+    x = _buff;                                           \
+  } while(0)
+
+
+
+
+void
+ragged2nameseq(const seqtype_t *s, const char **seq)
+{
+  seqtype_t ss;
+  int i = 0;
+
+  while(s->d[i] != -1) {
+    const arc_t **arcs;
+    
+    ss = *s;
+    ss.d[i] = -1;
+    arcs = ragged2arcs(&ss);
+
+    if (arcs && arcs[0]->arr) {
+      // array case
+      assert(s->d[i] < arcs[0]->arr->size);
+      ASPRINTF(seq[i],"[%d]",s->d[i]);
+    } else {
+      ASPRINTF(seq[i], ".%s", arcs[s->d[i]]->sym);
+    }
+    ++i;
+  }
+  seq[i] = NULL;
+}
 
 int
 main(int argc, char **argv)
@@ -23,7 +74,23 @@ main(int argc, char **argv)
       i++;
     }
   }
-    
+
+  for (int i=0; i<1000*100; ++i) {
+    chipaddr_t a=random() % 1000*1000*10, rem;
+    seqtype_t rp;
+    const char *seq[MAXDEPTH];
+
+    rem = addr2ragged(a, &rp);
+    print_ragged(&rp);
+    printf(" rem=%ld\n", rem);
+
+    ragged2nameseq(&rp, seq);
+
+    for (const char **s=seq; *s; ++s)
+      printf("%s", *s);
+    printf("\n");
+  }
+
     
 
   return 0;
