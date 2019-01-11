7e76f0a23ac9b161145b39d3148cd93c703370bb
Tomasz Nagler <thomas.j.nagler@intel.com>
2019-01-11 13:25:53 +0100

    Adding new standalone header files
    Change-Id: I92d3ae0f6b373e094eafc4c77ca17f81cc2f642e

---
+++ async-toolkit/m3utils/mby-merged/wm/src/main/c/mby_mac2par.h
@@ -0,0 +1,20 @@
+// -*- mode:c -*-
+
+// Copyright (C) 2019 Intel Corporation
+
+#ifndef MBY_MAC2PAR_H
+#define MBY_MAC2PAR_H
+
+#include "fm_types.h"
+
+#define MBY_PA_MAX_SEG_LEN  192 // max segment length
+
+typedef struct mbyRxMacToParserStruct
+{
+    fm_uint32        RX_PORT;                      ///< Ingress port
+    fm_uint32        RX_LENGTH;                    ///< Ingress packet data length [bytes]
+    fm_byte          SEG_DATA[MBY_PA_MAX_SEG_LEN]; ///< Ingress segment data
+
+} mbyRxMacToParser;
+
+#endif
