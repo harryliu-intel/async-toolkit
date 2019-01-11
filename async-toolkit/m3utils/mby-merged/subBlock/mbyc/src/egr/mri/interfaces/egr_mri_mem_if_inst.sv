c7ba0e405b2ebf62368643157c634e750ad6a5a3
Maeda Nunez, Luis Alfonso <luis.alfonso.maeda.nunez@intel.com>
2019-01-10 17:57:31 -0800

    Connected MRI memories using interface 1 to many

---
+++ async-toolkit/m3utils/mby-merged/subBlock/mbyc/src/egr/mri/interfaces/egr_mri_mem_if_inst.sv
@@ -0,0 +1,2 @@
+mby_mem_1r1w_if    #(.W_DATA(42),    .W_DEEP(4),   .W_INST(8) )    egr_mri_rreq_fifo_if();
+mby_mem_1r1w_if    #(.W_DATA(528),    .W_DEEP(4),   .W_INST(8) )    egr_mri_rrsp_fifo_if();
