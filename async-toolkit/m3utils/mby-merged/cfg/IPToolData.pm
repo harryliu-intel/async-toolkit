5fb4b5bc82fc13297e6d9f505fc0f56dc3afcf7a
Lopez Chin, Jesus A <jesus.a.lopez.chin@intel.com>
2019-02-03 12:56:01 -0800

    shdv pointer fixed

--- async-toolkit/m3utils/mby-merged/cfg/IPToolData.pm
+++ async-toolkit/m3utils/mby-merged/cfg/IPToolData.pm
@@ -187,8 +187,7 @@ $ToolConfig_ips{sva_lib_ip} = {
 };
 
 $ToolConfig_ips{shdv} = {
-#   PATH => "$ENV{IP_MODELS}/shdv/&get_tool_version()",
-   PATH => "/nfs/sc/disks/sc_mby_00091/jesusalo/mby/work_root/shdv",   
+   PATH => "$ENV{IP_MODELS}/shdv/&get_tool_version()",
    VERSION => "shdv-dev-x0-19ww02b",
    OTHER   => {
       SEARCH_PATHS   => ["&get_tool_path()",],
