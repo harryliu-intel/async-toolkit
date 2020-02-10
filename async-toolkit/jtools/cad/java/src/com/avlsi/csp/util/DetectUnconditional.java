a294d47641e10b26a39d0695cb68b3764cab34a8
Liu, Ruokun <harry.liu@intel.com>
2020-02-10 14:22:25 -0800

    Correctly enumerate and use maybe peeked channels.
    [git-p4: depot-paths = "//mrl/sw/intel/": change = 351544]

--- async-toolkit/jtools/cad/java/src/com/avlsi/csp/util/DetectUnconditional.java
+++ async-toolkit/jtools/cad/java/src/com/avlsi/csp/util/DetectUnconditional.java
@@ -1,8 +1,12 @@
 package com.avlsi.csp.util;
 
 import java.math.BigInteger;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import com.avlsi.csp.ast.*;
@@ -18,22 +22,48 @@ public class DetectUnconditional extends VisitorByCategory {
     /** Possibly peeked channels */
     private Set<String> peeked;
 
-    /** Definitely unconditional channels */
+    /** Definitely unconditional send/recv channels */
     private Set<String> uncond;
+
+    /** Definitely unconditional peeked channels */
+    private Set<String> uncondPeek;
 
     /** Resolve function calls to declarations */
     private final RefinementResolver resolver;
 
-    /** Whether encountered an array with a variable index */
-    boolean variableIndex = false;
+    /** For type information of channels */
+    private final Map<String,Type> portTypes;
 
-    /** Build up the name of the channel being accessed */
-    StringBuilder channelName = null;
+    private Type currType;
 
-    public DetectUnconditional(final RefinementResolver resolver) {
+    /** Possible channel names */
+    private List<StringBuilder> channelNames = null;
+
+    public class Results {
+        public Set<String> getPeeked() {
+            return peeked;
+        }
+
+        public Set<String> getUncond() {
+            return uncond;
+        }
+
+        public Set<String> getUncondPeek() {
+            return uncondPeek;
+        }
+    }
+
+    public DetectUnconditional(final RefinementResolver resolver,
+                               final Map<String,Type> portTypes) {
         this.resolver = resolver;
+        this.portTypes = portTypes;
         peeked = new HashSet<>();
         uncond = new HashSet<>();
+        uncondPeek = new HashSet<>();
+    }
+
+    public Results getResults() {
+        return new Results();
     }
 
     public Set<String> getPeeked() {
@@ -44,26 +74,39 @@ public class DetectUnconditional extends VisitorByCategory {
         return uncond;
     }
 
+    public Set<String> getUncondPeek() {
+        return uncondPeek;
+    }
+
     private Set<String> intersect(Set<String> a, Set<String> b) {
         final Set<String> result = new HashSet<>(a);
         result.retainAll(b);
         return result;
     }
 
-    private void nobranch(String name, boolean peek) {
-        if (name != null) {
-            uncond.add(name);
-            if (peek) peeked.add(name);
+    private void nobranch(List<StringBuilder> names, boolean peek) {
+        if (peek) {
+            for (StringBuilder name : names) {
+                peeked.add(name.toString());
+            }
+        }
+
+        if (names.size() == 1) {
+            if (peek) {
+                uncondPeek.add(names.get(0).toString());
+            } else {
+                uncond.add(names.get(0).toString());
+            }
         }
     }
 
-    private String getChannelName(final ExpressionInterface expr)
+    private List<StringBuilder> getChannelName(final ExpressionInterface expr)
         throws VisitorException {
-        channelName = new StringBuilder();
-        variableIndex = false;
+        channelNames = new ArrayList<>();
+        currType = null;
         expr.accept(this);
-        final String result = variableIndex ? null : channelName.toString();
-        channelName = null;
+        final List<StringBuilder> result = channelNames;
+        channelNames = null;
         return result;
     }
 
@@ -93,22 +136,40 @@ public class DetectUnconditional extends VisitorByCategory {
 
     public void visitArrayAccessExpression(ArrayAccessExpression e)
         throws VisitorException {
-        if (channelName == null) {
+        if (channelNames == null) {
             super.visitArrayAccessExpression(e);
         } else {
+            e.getArrayExpression().accept(this);
+
+            if (!(currType instanceof ArrayType)) {
+                throw new VisitorException("Expected an array");
+            }
+
+            final ArrayType at = (ArrayType) currType;
+            currType = at.getElementType();
             final BigInteger idx =
                 CspUtils.getIntegerConstant(e.getIndexExpression());
+            final BigInteger min;
+            final BigInteger max;
             if (idx == null) {
-                variableIndex = true;
+                min = CspUtils.getIntegerConstant(at.getRange().getMinExpression());
+                max = CspUtils.getIntegerConstant(at.getRange().getMaxExpression());
             } else {
-                e.getArrayExpression().accept(this);
+                min = idx;
+                max = idx;
+            }
+            final List<StringBuilder> oldChannelNames = channelNames;
+            channelNames = new ArrayList<StringBuilder>();
+            for (StringBuilder channelName : oldChannelNames) {
                 final int l = channelName.length();
                 if (channelName.charAt(l - 1) == ']') {
                     channelName.setCharAt(l - 1, ',');
                 } else {
                     channelName.append('[');
                 }
-                channelName.append(idx + "]");
+                for (BigInteger bi = min; bi.compareTo(max) <= 0; bi = bi.add(BigInteger.ONE)) {
+                    channelNames.add(new StringBuilder(channelName.toString() + bi + "]"));
+                }
             }
         }
     }
@@ -116,57 +177,84 @@ public class DetectUnconditional extends VisitorByCategory {
     public void visitIdentifierExpression(IdentifierExpression e)
         throws VisitorException {
         super.visitIdentifierExpression(e);
-        if (channelName != null) {
-            channelName.append(e.getIdentifier());
+        if (channelNames != null) {
+            currType = portTypes.get(e.getIdentifier());
+            channelNames.add(new StringBuilder(e.getIdentifier()));
         }
     }
 
     public void visitStructureAccessExpression(StructureAccessExpression e)
         throws VisitorException {
         super.visitStructureAccessExpression(e);
-        if (channelName != null) {
-            channelName.append('.' + e.getFieldName());
+        if (channelNames != null) {
+            if (currType instanceof ChannelStructureType) {
+                currType =
+                    ((ChannelStructureType) currType).getMemberType(e.getFieldName());
+            }
+            for (StringBuilder channelName : channelNames) {
+                channelName.append('.' + e.getFieldName());
+            }
         }
+    }
+
+    private void mergeBranch(List<Set<String>> accum) {
+        if (accum.get(0) == null) {
+            accum.set(0, uncond);
+            accum.set(1, uncondPeek);
+        } else {
+            accum.set(0, intersect(accum.get(0), uncond));
+            accum.set(1, intersect(accum.get(1), uncondPeek));
+        }
+    }
+
+    private void evaluateBranch(GuardedCommandInterface gci, List<Set<String>> accum)
+        throws VisitorException {
+        uncond = new HashSet<>();
+        uncondPeek = new HashSet<>();
+
+        ExpressionInterface guard = null;
+        StatementInterface command = null, guardStmt = null;
+        if (gci instanceof GuardedCommandWithStatement) {
+            final GuardedCommandWithStatement gcws =
+                (GuardedCommandWithStatement) gci;
+            guard = gcws.getGuard();
+            command = gcws.getCommand();
+            guardStmt = gcws.getGuardStatement();
+        } else if (gci instanceof GuardedCommand) {
+            final GuardedCommand gcws = (GuardedCommand) gci;
+            guard = gcws.getGuard();
+            command = gcws.getCommand();
+        }
+
+        if (guard != null) guard.accept(getVisitor());
+        if (guardStmt != null) guardStmt.accept(getVisitor());
+        if (command != null) command.accept(getVisitor());
+
+        mergeBranch(accum);
     }
 
     protected void
     processAbstractGuardedStatement(AbstractGuardedStatement s)
         throws VisitorException {
-        Set<String> oldUncond = uncond;
-        Set<String> merged = null;
-        for (Iterator i = s.getGuardedCommands(); i.hasNext(); ) {
-            uncond = new HashSet<>();
-            final GuardedCommandInterface gci =
-                (GuardedCommandInterface) i.next();
-            ExpressionInterface guard = null;
-            StatementInterface command = null, guardStmt = null;
-            if (gci instanceof GuardedCommandWithStatement) {
-                final GuardedCommandWithStatement gcws =
-                    (GuardedCommandWithStatement) gci;
-                guard = gcws.getGuard();
-                command = gcws.getCommand();
-                guardStmt = gcws.getGuardStatement();
-            } else if (gci instanceof GuardedCommand) {
-                final GuardedCommand gcws = (GuardedCommand) gci;
-                guard = gcws.getGuard();
-                command = gcws.getCommand();
-            }
+        final Set<String> oldUncond = uncond;
+        final Set<String> oldUncondPeek = uncondPeek;
 
-            if (guard != null) guard.accept(getVisitor());
-            if (guardStmt != null) guardStmt.accept(getVisitor());
-            if (command != null) command.accept(getVisitor());
-            if (merged == null) merged = uncond;
-            else merged = intersect(merged, uncond);
+        final List<Set<String>> accum = Arrays.asList(null, null);
+        for (Iterator i = s.getGuardedCommands(); i.hasNext(); ) {
+            evaluateBranch((GuardedCommandInterface) i.next(), accum);
         }
         final StatementInterface els = s.getElseStatement();
         if (els != null) {
             uncond = new HashSet<>();
+            uncondPeek = new HashSet<>();
             els.accept(getVisitor());
-            if (merged == null) merged = uncond;
-            else merged = intersect(merged, uncond);
+            mergeBranch(accum);
         }
+
         uncond = oldUncond;
-        uncond.addAll(merged);
+        uncondPeek = oldUncondPeek;
+        uncond.addAll(accum.get(0));
+        uncondPeek.addAll(accum.get(1));
     }
 
     public void visitFunctionCallExpression(FunctionCallExpression e)
