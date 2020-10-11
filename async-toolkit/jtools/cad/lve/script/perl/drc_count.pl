b053afc922560378a4cbdfe6f46a65e665d89cdf
Andrew Lines <andrew.lines@intel.com>
2020-10-11 11:30:32 -0800

    Add drc_count script to lve.
    [git-p4: depot-paths = "//mrl/sw/intel/": change = 355321]

---
+++ async-toolkit/jtools/cad/lve/script/perl/drc_count.pl
@@ -0,0 +1,20 @@
+#!/usr/intel/bin/perl -w
+
+my %count;
+my $rule;
+while (my $line = <STDIN> ){
+    if ($line =~ /^ (\S+):/) { $rule = $1; }
+    if ($line =~ /(\d+) violations found.$/) {
+        my $c=$1;
+        print "$rule\t$c\n";
+        if ($rule =~ /^M/ || $rule =~ /^V/) { $class="metvia"; }
+        else                                { $class="other"; }
+        if (defined($count{$class})) { $count{$class}+=$c; }
+        else                         { $count{$class} =$c; }
+    }
+}
+
+print "\n";
+foreach my $key (keys %count) {
+    print "$key violations: $count{$key}\n";
+}
