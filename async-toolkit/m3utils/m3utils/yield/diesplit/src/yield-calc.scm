ffda79b2d9b6a5f857984ca6090db3ed27c3a302
Mika Nystrom <mika.nystroem@intel.com>
2020-12-30 13:18:03 -0800

    moved print

--- async-toolkit/m3utils/m3utils/yield/diesplit/src/yield-calc.scm
+++ async-toolkit/m3utils/m3utils/yield/diesplit/src/yield-calc.scm
@@ -1,13 +1,6 @@
 (load "term-defs.scm")
 (load "term.scm")
 
-(define (choose n k)
-  (let loop ((d 1)             ;; Denominator
-             (u (+ n (- k) 1)) ;; nUmerator
-             (p 1)             ;; Product
-             )
-    (if (> d k) (begin (dis "choose: " (round p) dnl) (round p))
-        (loop (+ d 1) (+ u 1) (* p (/ u d))))))
 
 (define (area-yield A)
   (make-power-term A))
