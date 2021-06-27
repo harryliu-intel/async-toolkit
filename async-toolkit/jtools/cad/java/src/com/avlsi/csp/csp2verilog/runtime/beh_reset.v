e5bb51f4fc11904469ef804eb61df9ef6940f880
Liu, Ruokun <harry.liu@intel.com>
2021-06-27 13:32:06 -0800

    Set DLY pin based on runtime command line argument.
    [git-p4: depot-paths = "//mrl/sw/intel/": change = 357543]

--- async-toolkit/jtools/cad/java/src/com/avlsi/csp/csp2verilog/runtime/beh_reset.v
+++ async-toolkit/jtools/cad/java/src/com/avlsi/csp/csp2verilog/runtime/beh_reset.v
@@ -8,6 +8,16 @@ module beh_reset #(parameter RESETS = 1, STARTS = 0, DELAYS = 0)
                   (output logic [RESETS+STARTS+DELAYS-1:0] reset_n);
 initial begin
   reset_n = '0;
+  if (DELAYS > 0) begin
+    int dly = 0;
+    if ($test$plusargs("DLY")) begin
+      $value$plusargs("DLY=%d", dly);
+      $display("%m: DLY set to %d", dly);
+    end
+    for (int i = RESETS+STARTS; i < RESETS+STARTS+DELAYS; i++) begin
+      reset_n[i] = dly[0];
+    end
+  end
   #`CAST2VERILOG_RESET_DURATION;
   reset_n[RESETS-1:0] = '1;
   if (STARTS > 0) begin
