61a8a4518d15c6ed8c46542e7e955ff43393cf38
Mika Nystrom <mika.nystroem@intel.com>
2021-07-14 18:18:33 -0700

    example program for external trace writing

---
+++ async-toolkit/m3utils/m3utils/spice/ct/src/ext_write.sh
@@ -0,0 +1,1 @@
+/nfs/sc/disks/bfn_cb_05/mnystroe/m3utils/spice/ct/AMD64_LINUX/ct -fsdb /nfs/sc/disks/bfn_cb_05/mnystroe/m3utils/spice/fsdb/src/nanosimrd -threads 20 -wthreads 10 -r vdd -wrtrace /nfs/sc/disks/bfn_cb_05/mnystroe/m3utils/spice/ctlib/tracewrite/AMD64_LINUX/tracewriter /nfs/sc/disks/bfn_cb_05/mnystroe/d2d/2021ww24/cloudbreak/testbench/mnystroe-result-fsdb.tr0.fsdb full
