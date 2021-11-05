aa906b76d814e09a2f4ed3954e93d1be4d05918e
Mika Nystrom <mika.nystroem@intel.com>
2021-11-05 07:50:24 -0700

    yield calcs 11/4/2021 per Anurag's requests a good deal of arbitrary area scaling has been done, to match Anurag's area numbers

--- async-toolkit/m3utils/m3utils/yield/diesplit/src/reports-21ww45.scm
+++ async-toolkit/m3utils/m3utils/yield/diesplit/src/reports-21ww45.scm
@@ -11,7 +11,7 @@
 ;; N5 and N3 values manually.
 ;;
 
-
+(load "reports-21ww38.scm")
         
 (define (n5-core-model)
   '(ftr-core
@@ -93,7 +93,7 @@
                        ,(extract-labeled-block 'hard-io (n5-core-model))
                         (soft-io
                          (* #f 8 8 (* mac-spare 16 16 (onemac 0.9591)))))
-                     (/ 1 8)))
+                     (* (/ 58 52.66)(/ 1 8))))
    
 
 (define (run-reports)
@@ -108,4 +108,148 @@
   (report-yields-for-params (n3-ftr-tiled-split-die-model) *n3-params* '())
   )
 
+(define *n5-dols-per-mm2* 0.217)
+(define *n3-dols-per-mm2* 0.362)
 
+(define (simple-si-cost cost-per-mm2
+                        area
+                        yield
+                        need-per-prod)
+
+  (let* ((tgt-dice    (* need-per-prod 100))
+         (make-dice   (round (/ tgt-dice yield)))
+         (markup      1.03) ;; always?
+         (si-cost (round (* markup (/ (* make-dice cost-per-mm2 area) 100))))
+         
+         )
+
+    (dis "tgt-dice " tgt-dice dnl
+         "make-dice " make-dice dnl
+         "tot-si-cost " si-cost dnl)
+    si-cost
+    ))
+
+(define (scale-to-area model area)
+  (let* ((orig-area (compute-total-area model '()))
+         (factor (/ area orig-area)))
+
+    (make-scaled-model (string->symbol (string-append "scaled"
+                                                      (stringify factor)))
+                       model
+                       factor)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; cost of N5 tile
+(simple-si-cost *n5-dols-per-mm2* 58 0.8396 8)
+
+;; N3 monolithic 51.2T
+
+(report-yields-for-params
+ (scale-to-area (n3-ftr-split-die-model) 483)
+ *n3-params*
+ '())
+
+(simple-si-cost *n3-dols-per-mm2* 483 0.332 2)
+
+;; N3 halfcore
+
+(report-yields-for-params (scale-to-area (n3-ftr-tiled-split-die-model) 396) *n3-params* '())
+
+(simple-si-cost *n3-dols-per-mm2* 396 0.370 2)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; 102.4T
+;;
+
+;; falcon tile yield
+(report-yields-for-params (make-scaled-model `n3-falcon-tile (convert-n5-to-n3 (n5-io-tile-model)) (/ 82 50.04)) *n3-params* '())
+
+;; cost of Falcon N3 tile
+(simple-si-cost *n3-dols-per-mm2* 81 0.8100 8)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (n5-core-102p4-model)
+  ;; this is kind of synthetic, to match N3 monolithic (unbuildable) design
+  '(ftr-102p4-core
+    (hard-io 423.7 serdes-area 423.7)
+    
+    (soft-io
+     (* #f 8 8 (* mac-spare 33 32 (onemac 0.9591))))
+    ;; is this right?  where are the channels?
+    
+    (fwd 
+     
+     (ppu-stm-tcm 328.34)
+     
+     (parde 41.12 ram-area 13.6)
+     
+     (pkt-body-learn 9.01)
+     )
+    
+    (tm
+     
+     (* #f 4 4 (pkt-buffer-sram (* cdm-spare 193 192
+                                   (cdm-block 0.382 ram-area 0.382))))
+     ;; pktbuffer from Anurag
+     
+     (queueing 102.9 ram-area 60 channel-area 16)
+     ;; TM from Anurag, ram-area guessed, channel-area GPIO from before?
+     )
+    
+    (misc     26.2  ram-area 6.6 channel-area 6.6)
+    ;; scaled 
+
+    (space 44.62 repair 1 repair-cost 0.0)
+    )
+  )
+
+(define (n5-ftr-102p4-split-model)
+  `(ftr-split-102p4
+    ,(make-scaled-model `half-ftr-102p4 (n5-core-102p4-model) 0.5)
+
+        (split-d2d 29.84 serdes-area 29.84)             ;; from Anurag
+    )
+  )
+
+(define (n3-ftr-102p4-split-model)
+  (scale-to-area (convert-n5-to-n3 (n5-ftr-102p4-split-model)) 680))
+
+(report-yields-for-params (n3-ftr-102p4-split-model) *n3-params* '())
+
+;; just the core
+
+
+;;N5
+(define (n5-ftr-102p4-split-core-model)
+  (scale-to-area `(ftr-split-core-102p4
+                   (io-d2d 20 serdes-area 20)
+                   ,(remove-labeled-block
+                     'hard-io
+                     (remove-labeled-block
+                      'soft-io
+                      (n5-ftr-102p4-split-model))))
+                 556))
+
+(simple-si-cost *n5-dols-per-mm2* 556 0.380 2)
+
+;; N3
+;; some of these scale-to-area efforts are troublingly large,
+;; fairly big discrepancy between Anurag's area numbers and what comes
+;; out of the converter
+(report-yields-for-params (scale-to-area (convert-n5-to-n3 (n5-ftr-102p4-split-core-model)) 480) *n3-params* '())
+
+(simple-si-cost *n3-dols-per-mm2* 480 0.356 2)
+
+(define (tally-cost si-costs)
+  (let* ((si-sum (apply + si-costs))
+         (pkg    150)
+         (packaged (+ si-sum pkg))
+         (test    20)
+         (tested   (+ test packaged))
+         (yield 0.98)
+         (out-the-door (/ tested yield)))
+    out-the-door))
