433cd01412108732d6217259099f9a497518bef0
Michael Wrighton <michael.wrighton@intel.com>
2022-03-07 15:04:15 -0800

    enable lamb generation from a filelist

--- async-toolkit/m3utils/applications.design-automation.memory.lamb/gtr/tcl/batchLambs.tcl
+++ async-toolkit/m3utils/applications.design-automation.memory.lamb/gtr/tcl/batchLambs.tcl
@@ -1,4 +1,4 @@
-#!/usr/intel/bin/tclsh
+#!/usr/intel/pkgs/tcl-tk/8.6.8/bin/tclsh
 
 # Generate Netbatch Feeder File to produce a collection of LAMBs
 proc produceLambs { lambList {taskname lambgen } } {
@@ -32,11 +32,21 @@ proc produceLambs { lambList {taskname lambgen } } {
    puts "Once running, to see progress: https://nbflow.intel.com"
 }
 
+proc parseLambfile { lf } {
+   set if [open $lf r]
+   set lamblist [list]
+   while {[gets $if line]>=0} {   
+      set vals [split $line]
+      set l [dict create]
+      dict set l type [lindex $vals 0]
+      dict set l width [lindex $vals 1]
+      dict set l depth [lindex $vals 2]
+      lappend lamblist $l
+   }
+   return $lamblist
+}
 
-set cheetahProject tfc
-set cheetahConfig tfc_n5
-set nbqueue $::env(EC_SITE)_normal
-set qslot /bfn/fe
+
 
 proc lambRange { typelist minWidth minDepth maxWidth maxDepth } {
   set lamblist [list]
@@ -53,9 +63,43 @@ proc lambRange { typelist minWidth minDepth maxWidth maxDepth } {
   }
   return $lamblist
 }
+
+set cheetahProject tfc
+set cheetahConfig tfc_n5
+set nbqueue $::env(EC_SITE)_normal
+set qslot /bfn/fe
+
+package require cmdline
+set options {
+   {lf.arg  "" "build lambs from given file list"}
+   {mindepth.arg  4  "use specified minimum depth"}
+   {maxdepth.arg  4  "use specified minimum depth"}
+   {minwidth.arg  4  "use specified minimum depth"}
+   {maxwidth.arg  4  "use specified minimum depth"}
+
+}
+set usage ": batchLambs.tcl \[options]\noptions:"
+
+try {
+   array set params [::cmdline::getoptions argv $options $usage]
+   # Note: argv is modified now. The recognized options are
+	# removed from it, leaving the non-option arguments behind.
+   } trap {CMDLINE USAGE} {msg o} {
+   # Trap the usage signal, print the message, and exit the application.
+   # Note: Other errors are not caught and passed through to higher levels!
+	puts $msg
+	exit 1
+}
+
+if {  [string length $params(lf)] > 0 } {
+   set lf $params(lf)
+   puts "Producing lambs specified by $lf"
+   set lamblist [parseLambfile $lf]
+} else {
+  puts "Producing lambs specified by range"
+  set lamblist [lambRange { 1r1w1c } $params(mindepth) $params(minwidth) $params(maxdepth) $params(maxwidth)]
+}
+
 set wwdate [exec workweek -f %IYWW%IW.%w_%H.%M ]
 set taskname lambgen_$::env(USER)_${wwdate}
-
-# todo, provide better parameterization over expected 'set' of lambs instead of simple range
-set lamblist [lambRange { 1r1w1c } 2 4 10 10]
 produceLambs $lamblist $taskname
