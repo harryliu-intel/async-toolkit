f5ecc312bf0d3a6618a7091821d267aef06f863f
Andrew Lines <andrew.lines@intel.com>
2022-06-08 12:29:20 -0800

    Add cdl2sp script for new Laygen.  Generate gds.sp as well as gds.lgf.
    [git-p4: depot-paths = "//mrl/sw/intel/": change = 361201]

---
+++ async-toolkit/jtools/cad/external-tools-integration/cadence/virtuoso/script/perl/cdl2sp.pl
@@ -0,0 +1,219 @@
+#!/usr/intel/bin/perl -w
+
+# Translate CDL syntax to SP syntax for LayGen
+# Folds transistors
+# Copyright 2019 Intel
+# Authors: Andrew Lines
+
+use FileHandle;
+use IPC::Open2;
+use POSIX;
+use Data::Dumper qw(Dumper);
+
+# Output record separator
+$" = " ";
+
+# options and defaults
+$grid     = 1e-9;     # convert to nm
+$length   = 14e-9;    # transistor length
+$gridW    = 30e-9;    # rounding grid for width
+$maxNmosW = 3*$gridW; # maximum fold width for NMOS
+$maxPmosW = 3*$gridW; # maximum fold width for PMOS
+
+my $node_num=1;
+my %map_node;
+
+# usage banner
+sub usage() {
+    die "Usage: $0\n" .
+        " [--length=$length] [--gridW=$gridW]\n" .
+        " [--maxNmosW=$maxNmosW] [--maxPmosW=$maxPmosW]\n" .
+        " <top_cell> <cdl_in> <sp_out>\n";
+}
+
+# parse arguments
+while (defined $ARGV[0] && $ARGV[0] =~ /^--(.*)=(.*)/) {
+    $flag = $1;
+    $val = $2;
+    if    ($flag eq "length")   { $length = $val; }
+    elsif ($flag eq "grid")     { $grid = $val; }
+    elsif ($flag eq "gridW")    { $gridW = $val; }
+    elsif ($flag eq "maxNmosW") { $maxNmosW = $val; }
+    elsif ($flag eq "maxPmosW") { $maxPmosW = $val; }
+    else { usage(); }
+    shift @ARGV;
+}
+@ARGV == 3 or usage();
+my $top   = "$ARGV[0]";
+my $f_in  = "$ARGV[1]";
+my $f_out = "$ARGV[2]";
+
+# report a fatal error
+sub error_msg {
+    die "$0: $f_in, line $.: $_[0]\n";
+}
+
+# Translate top cell name to library format
+my $newtop = $f_out;
+$newtop =~ s/\.sp$//g;
+
+# Do linewise translation of CDL to SNP
+open IN,  "<$f_in"  or die "Can't read $f_in\n";
+$line = <IN>;
+$. = 0;
+my $cell;
+my @out;
+my %portnodes;
+my %localnodes;
+my %g_nodes;
+my %sd_nodes;
+my %b_nodes;
+my $num_mos = 0;
+while ($line) {
+    $next_line = <IN>;
+    while (defined $next_line && $next_line =~ s/^\+/ /) {
+        chomp $line;
+        $line .= $next_line;
+        $next_line = <IN>;
+    }
+    my $full = $line;
+    if ($line =~ s/^\.SUBCKT\s+//i || $line =~ s/^\.SUBCIRCUIT\s+//i) {
+
+        # Begin Subcircuit Definition
+        my @parameters = ();
+
+        $line =~ s/(\S+)\s+//;
+        $cell = $1;
+        while ($line =~ s/(\S+)\s*//) {
+            $arg = $1;
+            if ($arg =~ /(\S+)=(\S+)/) {
+                # Ignore parameters
+            } else {
+                # Node argument
+                $portnodes{$arg}=1 if ($cell eq $top);
+            }
+        }
+
+    } elsif ($line =~ s/^\.ENDS//i) {
+
+        $cell = "";
+
+    } elsif ($line =~ s/^M//i) {
+
+        # MOSFET
+        my %parameters = ();
+        my @nodes = ();
+
+        $line =~s/^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)//
+            or error_msg "Missing arguments in mosfet: $full";
+        my $name   = $1;
+        my $type   = $6;
+        my $drain  = $2;
+        my $gate   = $3;
+        my $source = $4;
+        my $bulk   = $5;
+        $drain  = rename_net($drain);
+        $gate   = rename_net($gate);
+        $source = rename_net($source);
+        $bulk   = rename_net($bulk);
+        $drain  =~ s/#//g;
+        $source =~ s/#//g;
+        $gate   =~ s/#//g;
+        $bulk   =~ s/#//g;
+        if ($cell eq $top) {
+            $localnodes{$drain}=1;
+            $localnodes{$source}=1;
+            $localnodes{$gate}=1;
+            $localnodes{$bulk}=1;
+            $g_nodes{$gate}=1;
+            $sd_nodes{$drain}=1;
+            $sd_nodes{$source}=1;
+            $b_nodes{$bulk}=1;
+        }
+
+        # create parameter list in aspice order
+        while ($line =~ s/(\S+)=//) {
+            my $p = lc($1);
+            my $v;
+            if ($line =~ s/'([^']*)'\s+//) {
+                $v = $1;
+            } elsif ($line =~ s/(\S+)\s+//) {
+                $v = $1;
+            }
+            $parameters{$p} = $v;
+        }
+
+        # subsitute some constants
+        $parameters{"w"} =~ s/DIFF_PITCH/$gridW/g;
+        $parameters{"w"} = eval($parameters{"w"});
+        $parameters{"l"} =~ s/TRANSISTOR_LENGTH/$length/g;
+        $parameters{"l"} = eval($parameters{"l"});
+        my $x = $parameters{"w"};
+        my $y = $parameters{"l"};
+        $x *= $parameters{"m"} if (defined($parameters{"m"}));
+
+        # compute folds
+        my $w = POSIX::floor($parameters{"w"}/$gridW+0.5);
+        my $maxW = $maxPmosW;
+        $maxW = $maxNmosW if ($type =~ /^n/);
+        $maxW = POSIX::floor($maxW/$gridW+0.5);
+        my $folds = POSIX::ceil($w/$maxW);
+        my $small = 0;
+        my $mod = $w%$maxW;
+        if ($mod!=0) { $small = $maxW - $mod; } # number of small folds
+
+        # emit
+        for ($f=0; $f<$folds; $f++) {
+            if ($folds>1) { $parameters{"w"} = ($maxW-($f<$small)) * $gridW; }
+            $w2 = $parameters{"w"}/$grid;
+            $l2 = $parameters{"l"}/$grid;
+            $type =~ /^(.)/;
+            my $tc = $1;
+            if ($cell eq $top) {
+                push @out, "M${num_mos} $drain $gate $source $bulk " .
+                    "$type w=${w2}n l=${l2}n\n";
+                $num_mos++;
+            }
+        }
+
+    } elsif ($line =~ s/^X//i) {
+        # subcells not supported
+        error_msg "cdl2sp expects flat transistors";
+    } elsif ($line =~ m/^\*/) {
+        # comment line, do nothing
+    } elsif ($line =~ s/^\s*\n//i ) {
+        # empty line with space, do nothing
+    } else {
+        # unknown line, error
+        error_msg "Unknown line type: $line";
+    }
+    $line = $next_line;
+}
+close IN;
+
+# output
+open OUT, ">$f_out" or die "Can't write $f_out\n";
+print OUT "* Generated by cdl2sp from $top\n";
+print OUT ".SUBCKT $newtop";
+foreach my $node (sort keys %portnodes) {
+    print OUT " $node";
+}
+print OUT "\n";
+foreach my $line (@out) {
+    print OUT $line;
+}
+print OUT ".ENDS\n";
+close OUT;
+
+# rename internal nodes that end in #
+sub rename_net {
+    my ($net) = (@_);
+    if (defined $map_node{$net}) { $net = $map_node{$net}; }
+    elsif ($net =~ /(\S+)#/g) {
+        my $new_net = "n${node_num}";
+        $map_node{$net}=$new_net;
+        $node_num++;
+        $net=$new_net;
+    }
+    return $net;
+}
