707709d39d40eb32289ebbfeb37b8f6067580937
Mika Nystrom <mika.nystroem@intel.com>
2022-08-29 18:17:13 -0700

    added partial deriv

--- async-toolkit/m3utils/m3utils/vtsensor/src/example.scm
+++ async-toolkit/m3utils/m3utils/vtsensor/src/example.scm
@@ -29,3 +29,69 @@
 (define pts (get-points))
 
 (define (eval-estimator) (TriOsc.EvalEstimator temp-meshes *samples* *k* pts 1))
+
+(define p (TriOsc.Estimate test-point temp-meshes *samples* *k*))
+
+(define (partial-1 a b rel-step)
+  ;; compute a single partial difference
+  (list (assoc*- 'temp (/ rel-step) b a)
+        (assoc2*- 'V (/ rel-step) b a)))
+
+(define (partial-deriv test-point temp-meshes samples k rel-step)
+
+  (define (estimate p)
+    (TriOsc.Estimate p temp-meshes samples k))
+
+  (let* ((e0 (estimate test-point))
+         (px (make-advances test-point rel-step))
+         (ex (map estimate px))
+         (f (lambda(e)(partial-1 e0 e rel-step)))
+         )
+
+    (map f ex)
+
+    )
+  )
+
+
+(define (assoc*- key m a b)
+  (cons key
+        (* m
+           (- (cdr (assoc key a))
+              (cdr (assoc key b))))))
+
+(define (assoc2*- key m a b)
+  (define (mult x) (* m x))
+  
+  (cons key
+        (let* ((x (map mult (map -
+                                 (map cdr (cdr (assoc key a)))
+                                 (map cdr (cdr (assoc key b))))))
+               (y (map car (cdr (assoc key a)))))
+          (map cons y x))))
+
+
+
+(define (make-advances p rel-step)
+  ;; make all the steps in every direction
+  
+  (define (modify-point p rel-step k)
+    ;; modify kth dimension of p by multiplying by (+ 1 rel-step)
+    (let loop ((pp p)
+               (i 0)
+               (res '()))
+      (cond ((null? pp) (reverse res))
+            ((= i k)   (loop (cdr pp) (+ i 1)
+                             (cons (* (+ 1 rel-step) (car pp))
+                                   res)))
+            (else (loop (cdr pp) (+ i 1) (cons (car pp) res))))))
+                        
+
+  (let loop ((n (length p))
+             (res '()))
+    (if (= n 0)
+        res
+        (loop (- n 1)
+              (cons (modify-point p rel-step (- n 1)) res)))))
+
+
