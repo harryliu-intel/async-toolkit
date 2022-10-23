965e122842d7d972cf01f705a1e7272cb92a96e3
Mika Nystrom <mika.nystroem@intel.com>
2022-10-23 10:36:54 -0700

    cut back sims a bit, they run too long

--- async-toolkit/m3utils/m3utils/spice/techc/src/maketask.sh
+++ async-toolkit/m3utils/m3utils/spice/techc/src/maketask.sh
@@ -18,10 +18,15 @@ step=4
 # we get about 4 CPUs per machine?  6 ought to be more than enough, 4 might be optimal?
 
 corners="ss tt ff"
+xor_corners="tt"
+aoi_corners="tt"
 
 temps="-40 -20 0 25 50 75 85 100 125"
+xor_temps="50 75"
+aoi_temps="25 50 75 85 100 125"
 
-volts="0.11 0.13 0.15 0.17 0.19 0.21 0.23 0.25 0.27 0.29 0.31 0.33 0.35 0.40 0.45 0.50 0.55 0.60 0.65 0.70 0.75 0.80 0.85 0.90 0.95 1.0"
+buf_volts="0.11 0.13 0.15 0.17 0.19 0.21 0.23 0.25 0.27 0.29 0.31 0.33 0.35 0.40 0.45 0.50 0.55 0.60 0.65 0.70 0.75 0.80 0.85 0.90 0.95 1.0"
+volts="0.11 0.13 0.15 0.17 0.19 0.21 0.23 0.25 0.27 0.29 0.31 0.33 0.35 0.40 0.45 0.50"
 
 #techs="n5 1276p4 n3 n3e"
 techs="n5 1276p4 n3e 1278p3"
@@ -43,10 +48,13 @@ paras="true"
 #gates="xor buf aoi"
 gates="xor buf aoi"
 
+runmode="default"
+
 if [ "$1" == "-quick" ]; then
     # quick simulation, just to test
     # note all technologies are included
-    volts="0.29 0.90"
+    runmode="override"
+    volts="0.29 0.40 0.60 0.90"
     temps="50 125"
     modes="dyn"
     paras="true false"
@@ -55,10 +63,12 @@ if [ "$1" == "-quick" ]; then
 fi
 
 if [ "$1" == "-1278p3" ]; then
+    runmode="override"
     techs="1278p3"
 fi
 
 if [ "$1" == "-gates" ]; then
+    runmode="override"
     volts="0.40"
     temps="50"
     modes="dyn"
@@ -90,6 +100,19 @@ EOF
 tasknum=0
 
 for gate in ${gates}; do
+
+    if [ "${runmode}" == "default" ]; then
+        if   [ "${gate}" == "xor" ]; then
+            temps=${xor_temps}
+            corners=${xor_corners}
+        elif [ "${gate}" == "aoi" ]; then
+            temps=${aoi_temps}
+            corners=${aoi_corners}
+        elif [ "${gate}" == "buf" ]; then
+            volts=${buf_volts}
+        fi
+    fi
+    
 for para in ${paras}; do
 for corn in ${corners}; do
 for mode in ${modes}; do
