c04a43e649ba313a3c094a92dd4f3cff125dfb75
Mika Nystrom <mika.nystroem@intel.com>
2022-11-15 17:19:59 -0800

    update code

--- async-toolkit/m3utils/m3utils/spice/spicetiming/src/run.sh
+++ async-toolkit/m3utils/m3utils/spice/spicetiming/src/run.sh
@@ -2,7 +2,7 @@
 ROOT=../../BTC_STAGES.88f.xa
 
 QUICK=
-QUICK=-quick
+#QUICK=-quick
 PLOT="-graph 10"
 
 ../AMD64_LINUX/spicetiming -t ${ROOT}/xa -i ${ROOT}/flat.cdl_gds2 -root core_D_crypto_D_bitcoin_D_stage_D_TEST_U_BTC_U_STAGES_D_1000 -vdd 0.325 ${QUICK} ${PLOT}
