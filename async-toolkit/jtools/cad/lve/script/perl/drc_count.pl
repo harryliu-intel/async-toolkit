250da57add4ba490c39663fbd224fd842f2e9ca3
Andrew Lines <andrew.lines@intel.com>
2023-04-26 14:45:45 -0800

    Classify ^ZM* as metvia violation too.
    [git-p4: depot-paths = "//mrl/sw/intel/": change = 365241]

--- async-toolkit/jtools/cad/lve/script/perl/drc_count.pl
+++ async-toolkit/jtools/cad/lve/script/perl/drc_count.pl
@@ -7,7 +7,7 @@ while (my $line = <STDIN> ){
     if ($line =~ /(\d+) violation[s]* found.$/) {
         my $c=$1;
         print "$rule\t$c\n";
-        if ($rule =~ /^M/ || $rule =~ /^V/) { $class="metvia"; }
+        if ($rule =~ /^ZM/ || $rule =~ /^M/ || $rule =~ /^V/) { $class="metvia"; }
         else                                { $class="other"; }
         if (defined($count{$class})) { $count{$class}+=$c; }
         else                         { $count{$class} =$c; }
