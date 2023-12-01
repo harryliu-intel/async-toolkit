0a5e2250429238593e5a390c7dcb268f4cf8282f
Mika Nystrom <mika.nystroem@intel.com>
2023-11-30 16:17:51 -0800

    add this for documentation

--- async-toolkit/m3utils/rechar_flow_tmpl/env.zsh
+++ async-toolkit/m3utils/rechar_flow_tmpl/env.zsh
@@ -64,6 +64,9 @@ if [[ "${do_special_upf}" == "1" ]]; then
 
     upfdir=/nfs/site/disks/zsc9_fwr_sd_001/mnystroe/p1278_3x0p9eu1/2023ww43d5/models_core_hspice/m14_2x_1xa_1xb_6ya_2yb_2yc__bm5_1ye_1yf_2ga_mim3x_1gb__bumpp
 
+    # this one fails config:
+#    upfdir=/p/hdk/cad/pdk/pdk783_r0.9_23ww42.5/models/core/hspice/m14_2x_1xa_1xb_6ya_2yb_2yc__bm5_1ye_1yf_2ga_mim3x_1gb__bumpp/
+
     
     upfcorner=tttt
 
