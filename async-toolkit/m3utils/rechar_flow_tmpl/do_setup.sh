93932be8cfb500e4f1e6dd238e07029a5cdbc856
Mika Nystrom <mika.nystroem@intel.com>
2023-12-06 15:10:13 -0800

    link siliconsmart

--- async-toolkit/m3utils/rechar_flow_tmpl/do_setup.sh
+++ async-toolkit/m3utils/rechar_flow_tmpl/do_setup.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+# set up the WARD for running Siliconsmart
+# variables need to be set before sourcing this file
+
 rm -f cell_list
 ln -s ${cells} cell_list
 
 mkdir -p ${workdir}
+rm siliconsmart
+ln -s ${workdir} siliconsmart
 
 rm -f env.zsh
 ln -s ${env} env.zsh
