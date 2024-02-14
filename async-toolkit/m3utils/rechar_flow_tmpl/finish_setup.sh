12c10a8d8b4abf4728661245b57a1c420fdd6295
Mika Nystrom <mika.nystroem@intel.com>
2024-02-14 14:14:31 -0800

    remove some sources of screwups

--- async-toolkit/m3utils/rechar_flow_tmpl/finish_setup.sh
+++ async-toolkit/m3utils/rechar_flow_tmpl/finish_setup.sh
@@ -1,8 +1,16 @@
 #!/bin/sh 
 
-echo ""
-echo "REMEMBER TO RUN, if appropriate:"
-echo ""
-echo "rm -rf $RECHAR_SIS_WORKDIR; mkdir  $RECHAR_SIS_WORKDIR"
-echo ""
+rmdir $RECHAR_SIS_WORKDIR || true
+if [ ! -d $RECHAR_SIS_WORKDIR ]; then
+	echo "Creating work dir $RECHAR_SIS_WORKDIR"
+	mkdir $RECHAR_SIS_WORKDIR
+else
+	echo ""
+	echo "Work dir $RECHAR_SIS_WORKDIR NOT EMPTY..."
+	echo ""
+	echo "REMEMBER TO RUN, if appropriate:"
+	echo ""
+	echo "rm -rf $RECHAR_SIS_WORKDIR; mkdir  $RECHAR_SIS_WORKDIR"
+	echo ""
+fi
 
