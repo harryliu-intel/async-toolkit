62a4ff5e2c3accb831302ab5f00a3aaac87e59b9
Mika Nystrom <mika.nystroem@intel.com>
2024-02-19 16:41:52 -0800

    remove du

--- async-toolkit/m3utils/rechar_flow_tmpl/monitorspace.zsh
+++ async-toolkit/m3utils/rechar_flow_tmpl/monitorspace.zsh
@@ -4,8 +4,11 @@ source rechar_setup_env.zsh
 
 while true; do
     dfdata=`df . | tail -1 | awk '{print $2 " " $3 " " $4 " "$4/$2}'`
-    dudata=`du -xs ${RECHAR_SIS_WORKDIR} | awk '{print $1}'`
-    output="`date +%s` $dfdata $dudata"
+#    dudata=`du -xs ${RECHAR_SIS_WORKDIR} | awk '{print $1}'`
+#    output="`date +%s` $dfdata $dudata"
+    output="`date +%s` $dfdata"
     echo ${output}
     echo ${output} >> monitorspace.out
+
+    sleep 10
 done
