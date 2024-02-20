32821b0432e359b2477d75cc5f2c84dc0c6a1f50
Mika Nystrom <mika.nystroem@intel.com>
2024-02-19 16:35:25 -0800

    new monitoring program

---
+++ async-toolkit/m3utils/rechar_flow_tmpl/monitorspace.zsh
@@ -0,0 +1,11 @@
+#!/usr/intel/bin/zsh
+
+source rechar_setup_env.zsh
+
+while true; do
+    dfdata=`df . | tail -1 | awk '{print $2 " " $3 " " $4 " "$4/$2}'`
+    dudata=`du -xs ${RECHAR_SIS_WORKDIR} | awk '{print $1}'`
+    output="`date +%s` $dfdata $dudata"
+    echo ${output}
+    echo ${output} >> monitorspace.out
+done
