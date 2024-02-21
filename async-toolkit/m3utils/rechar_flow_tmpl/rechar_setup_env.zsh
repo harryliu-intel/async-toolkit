a6493b144070b6e146d2d3253d2295ad748e681a
Mika Nystrom <mika.nystroem@intel.com>
2024-02-20 18:43:04 -0800

    check CWD is WARD

--- async-toolkit/m3utils/rechar_flow_tmpl/rechar_setup_env.zsh
+++ async-toolkit/m3utils/rechar_flow_tmpl/rechar_setup_env.zsh
@@ -3,6 +3,15 @@
 # source from zsh file to set up common environment for rechar
 #
 
+mycwd=`pwd`
+realcwd=`realpath $mycwd`
+realward=`realpath $WARD`
+
+if [[ "$realward" != "$realcwd" ]]; then
+	echo "WARD $realward != CWD $realcwd"
+	exit 1
+fi
+
 TOP=${WARD}
 
 source ${RECHAR_PVTS_ZSH}
