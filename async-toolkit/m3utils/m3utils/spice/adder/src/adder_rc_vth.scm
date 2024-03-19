d539364265515711a4f49abfb65c797530a6dcad
Mika Nystrom <mika.nystroem@intel.com>
2024-03-19 16:22:24 -0700

    update for new measurements

---
+++ async-toolkit/m3utils/m3utils/spice/adder/src/adder_rc_vth.scm
@@ -0,0 +1,8 @@
+;;
+;; here we can define some constants or other useful utility thingies
+;; they will get pulled in by the schemagraph program and you can then
+;; call them from the scheme function evaluation code
+;;
+
+(define Klatency 5.3)
+
