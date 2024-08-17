9451218a5d8b64457685917e3c0f3ff4d33267cf
Andrew Lines <andrew.lines@intel.com>
2024-08-17 12:01:32 -0800

    Better classification of DRC errors for i18a.
    [git-p4: depot-paths = "//mrl/sw/intel/": change = 369610]

--- async-toolkit/jtools/cad/lve/script/perl/drc_count.pl
+++ async-toolkit/jtools/cad/lve/script/perl/drc_count.pl
@@ -1,20 +1,24 @@
 #!/usr/intel/bin/perl -w
 
 my %count;
+my %rules;
 my $rule;
 while (my $line = <STDIN> ){
     if ($line =~ /^ (\S+):/) { $rule = $1; }
     if ($line =~ /(\d+) violation[s]* found.$/) {
         my $c=$1;
-        print "$rule\t$c\n";
-        if ($rule =~ /^ZM/ || $rule =~ /^M/ || $rule =~ /^V/) { $class="metvia"; }
-        else                                { $class="other"; }
-        if (defined($count{$class})) { $count{$class}+=$c; }
-        else                         { $count{$class} =$c; }
+        my $class;
+        if ($rule =~ /NODATA/) { $class="nodata"; }
+        elsif ($rule =~ /^bm/ || $rule =~ /^m/ || $rule =~ /^bv/ || $rule =~ /^v/) { $class="metvia"; }
+        else { $class="other"; }
+        if (defined($count{$class})) { $count{$class}+=$c; $rules{$class} .= "\t$rule\t$c\n"; }
+        else                         { $count{$class} =$c; $rules{$class}  = "\t$rule\t$c\n"; }
     }
 }
 
-print "\n";
-foreach my $key (keys %count) {
+foreach my $key ("nodata", "other", "metvia") {
+    defined($count{$key}) or next;
     print "$key violations: $count{$key}\n";
+    print "$rules{$key}";
+    print "\n";
 }
