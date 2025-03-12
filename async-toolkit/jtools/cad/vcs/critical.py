761d970b99c2b313d9306556d297f5d2318b71aa
Karmazin, Robert <robert.karmazin@intel.com>
2025-03-12 15:01:00 -0800

    Introducing sim_vcs: script that generates a verilog testbench, compiles and runs vcs simulation, and optionally opens waveforms
    [git-p4: depot-paths = "//mrl/sw/intel/": change = 371033]

---
+++ async-toolkit/jtools/cad/vcs/critical.py
@@ -0,0 +1,211 @@
+#!/usr/intel/bin/python
+
+import sys
+import os   
+import glob
+import argparse
+import pprint
+
+class OutputNode:
+    def __init__(self, name):
+        self.name = name
+        self.times = list()
+        self.last_idx = 0
+        self.worst_idx = 0
+
+    def __str__(self):
+        return "%s with %d transistions, worst case @ %d (idx=%d)" % (self.name, len(self.times), self.times[self.worst_idx][0], self.worst_idx)
+
+    def add_transition(self, output_time, input_name, input_time):
+        self.times.append((output_time, input_name, input_time))
+        #print("Adding transition to %s: %d caused %d (%d)" % (self.name, output_time, input_time, output_time - input_time))
+        if (output_time - input_time) > (self.times[self.worst_idx][0] - self.times[self.worst_idx][2]) or self.times[self.worst_idx][2] == 0:
+            self.worst_idx = self.last_idx
+        if len(self.times) > 1:
+            self.last_idx += 1
+    
+    def get_transition(self, time):
+        print("  Getting transition for %s at time %d" % (convert_vcs_to_apr_name(self.name), time))
+        for idx in range(self.last_idx, -1, -1):
+            #print("    Comparing times[%d] = %d" % (idx, self.times[idx][0]))
+            if self.times[idx][0] <= time:
+                self.last_idx = idx
+                print("      Found Critical transition %s: %d -> %d (%d)" % (convert_vcs_to_apr_name(self.times[idx][1]), self.times[idx][2], self.times[idx][0], self.times[idx][0] - self.times[idx][2]))
+                return (self.times[idx][1], self.times[idx][2])
+        print("No more transitions found for time %f" % time)
+        return (None, None)
+    
+    def get_worst_transition(self):
+        return (self.times[self.worst_idx][1], self.times[self.worst_idx][2])
+
+    def get_last_transition(self):
+        return (self.times[self.last_idx][1], self.times[self.last_idx][2])
+
+
+def convert_apr_to_vcs_name(apr_name):
+    apr_name = apr_name.strip()
+    vcs_name = "TESTBENCH.x._$0"
+    for tok in apr_name.split('/'):
+        if '.' in tok or '[' in tok or ']' in tok:
+            #Need to escape this token
+            tok = "\\" + tok + " "
+        vcs_name = vcs_name + "." + tok
+    #print("Converted %s to %s" % (apr_name, vcs_name))
+    return vcs_name
+
+def convert_vcs_to_apr_name(vcs_name):
+    vcs_name = vcs_name.replace("TESTBENCH.x._$0.", "")
+    apr_toks = list()
+    newtok = ""
+    for tok in vcs_name.split('.'):
+        if tok.startswith("\\"):
+            if tok.endswith(" "):
+                apr_toks.append(tok[1:-1])
+                newtok = ""
+            else:
+                newtok = tok[1:]
+            continue
+        if len(newtok) > 0:
+            if tok.endswith(" "):
+                newtok += ".%s" % tok[:-1]
+                apr_toks.append(newtok)
+                newtok = ""
+            else:
+                newtok += ".%s" % tok
+            continue
+        else:
+            apr_toks.append(tok)
+    apr_name = '/'.join(apr_toks)
+    #print("Converted %s to %s" % (vcs_name, apr_name))
+    return apr_name
+
+
+def cycle_check(path, cur_node, cur_time, verbose=False):
+    for node in reversed(path):
+        if cur_node == node[0]:
+            #print("  Cycle detected at %s: %d - %d = %d" % (cur_node, node[1], cur_time, node[1] - cur_time))
+            return (node[1] - cur_time)
+    return -1
+
+
+def parse_hist_file(hist_file):
+    """
+    Processes the transition history log file.
+
+    Args:
+        hist_file (file): The file of the transition history, which contains records
+                         of output and input data with their respective times.
+
+    The function reads the transition history file, parses each line to extract
+    output and input names along with their times, and prints this information.
+    """
+    node_dict = dict()
+    for line in hist_file:
+        line = line.strip()
+        (output_data,input_data) = line.split(':')
+        (output_name, output_time) = output_data.split('@')
+        (input_name, input_time) = input_data.split('@')
+        #print("%s %s %s %s" % (output_name, output_time, input_name, input_time))
+        if output_name not in node_dict:
+            node_dict[output_name] = OutputNode(output_name)
+        node_dict[output_name].add_transition(int(output_time), input_name, int(input_time))
+    return node_dict
+
+
+def parse_conn_file(conn_file):
+    """
+    Processes the connectivity file.
+
+    Args:
+        conn_file (str): The file of the connectivity information, which contains
+                         records input nodes and their fanin
+
+    The function reads the connectivity file, parses each line to extract input nodes
+    and their fanin information, returning a dictionary of input nodes and their fanin
+    """
+    conn_dict = dict()
+    for line in conn_file:
+        line = line.strip('\n')
+        if ':' in line:
+            (input_node,fanin_node) = line.split(':')
+        else:
+            (input_node,fanin_node) = line.split(' ')
+        #print(" Connectivity %s -> %s" % (input_node,fanin_node))
+        if "TESTBENCH" in input_node:
+            if input_node not in conn_dict:
+                conn_dict[input_node] = fanin_node
+            else:
+                print("WARNING: Duplicate input node %s" % convert_apr_to_vcs_name(input_node))
+        else:
+            if convert_apr_to_vcs_name(input_node) not in conn_dict:
+                conn_dict[convert_apr_to_vcs_name(input_node)] = convert_apr_to_vcs_name(fanin_node)
+            else:
+                print("WARNING: Duplicate input node %s" % convert_apr_to_vcs_name(input_node))
+    return conn_dict
+
+
+def critical_path(hist_name, connect_name, start, verbose=False):
+    #Read the transition history file
+    hist_file = open(hist_name, 'r')
+    node_dict = parse_hist_file(hist_file)
+    hist_file.close()
+
+    #Read the connectivity file
+    connect_file = open(connect_name, 'r')
+    conn_dict = parse_conn_file(connect_file)
+    connect_file.close()
+
+    if '@' in start:
+        (start_node, start_time) = start.split('@')
+        start_time = int(start_time)
+    else:
+        start_node = start
+        start_time = -1
+    if "TESTBENCH" not in start_node:
+        start_node = convert_apr_to_vcs_name(start_node)
+    print("Crtitical path starting at %s @ %d" % (start_node, start_time))
+    if start_node not in node_dict:
+        print("ERROR: Start node %s not found" % start_node)
+        return
+    else:
+        print(node_dict[start_node])
+        if start_time == -1:
+            (cur_node, cur_time) = node_dict[start_node].get_last_transition()
+        else:
+            (cur_node, cur_time) = node_dict[start_node].get_transition(start_time)
+
+    # Find the critical path
+    max_depth = 100
+    cur_depth = 0
+    path = list()
+    while cur_depth < max_depth:
+        cycle_time = cycle_check(path, cur_node, cur_time)
+        if cycle_time > 0:
+            print("Depth %2d: %d, %3d, %s +Cycle %d" % (cur_depth, cur_time, path[-1][1]-cur_time, convert_vcs_to_apr_name(cur_node), cycle_time))
+        else:
+            if cur_depth == 0:
+                print("Depth %2d: %d, %3d, %s" % (cur_depth, cur_time, 0, convert_vcs_to_apr_name(cur_node)))
+            else:
+                print("Depth %2d: %d, %3d, %s" % (cur_depth, cur_time, path[-1][1]-cur_time, convert_vcs_to_apr_name(cur_node)))
+        path.append((cur_node, cur_time))
+        cur_depth += 1
+        cur_node = conn_dict[cur_node]
+        (cur_node, cur_time) = node_dict[cur_node].get_transition(cur_time)
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Identify the critical path.')
+    parser.add_argument('hist_file', help="Pointer to the transition history file")
+    parser.add_argument('connect_file', help="Pointer to the netlist connectivity file")
+    parser.add_argument('start', help="Starting node at starting time")
+    parser.add_argument('-v', '--verbose', action="store_true", help="Verbose output")
+    args = parser.parse_args()
+
+    #Call the main function
+    critical_path(args.hist_file, args.connect_file, args.start, args.verbose)
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ async-toolkit/jtools/cad/vcs/sim_vcs.pl
@@ -0,0 +1,240 @@
+#!/usr/intel/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+use Pod::Usage;
+use File::Spec::Functions qw/:ALL/;
+use FindBin;
+use Cwd qw/abs_path/;
+use Cwd qw/chdir/;
+use File::Spec::Functions;
+use Scalar::Util qw(looks_like_number);
+
+#Make sure all the appropriate environment variables are defined
+die "ERROR: undefined \$SPAR_DIR\n"  unless (defined($ENV{SPAR_DIR}));
+die "ERROR: undefined \$CAST_PATH\n" unless (defined($ENV{CAST_PATH}));
+die "ERROR: undefined \$VERILOG_DIR\n" unless (defined($ENV{VERILOG_DIR}));
+die "ERROR: undefined \$SOURCE_VERILOG_DIR\n" unless (defined($ENV{SOURCE_VERILOG_DIR}));
+
+
+# Command line options for simulation
+my ($cell, $env, $level, $corner, $outdir, $simv, $tcl, $t_run, $fsdb, $verdi, @simv_args, $verbose, $help);
+# Command line options to pass through to gen_model
+my (@cast_defines, @c2v_args, @gen_args, @vcs_args);
+
+
+GetOptions("cell=s"      => \$cell,
+           "env=s"       => \$env, 
+           "level=s"     => \$level,
+           "corner=s"    => \$corner,
+           "outdir=s"    => \$outdir,
+           "simv=s"      => \$simv,
+           "tcl=s"       => \$tcl,
+           "t-run=s"     => \$t_run,
+           "define=s"    => \@cast_defines,
+           "c2v-args=s"  => \@c2v_args,
+           "gen-args=s"  => \@gen_args,
+           "vcs-args=s"  => \@vcs_args,
+           "simv-args=s" => \@simv_args,
+           "fsdb=s"      => \$fsdb,
+           "verdi!"      => \$verdi,
+           "verbose!"    => \$verbose,
+           "help!"       => \$help) || pod2usage(2);
+
+pod2usage(-verbose => 1) if $help;
+
+
+# If outdir is provided, check to make sure it exists
+my $wd;
+if ($outdir) {
+    #Working directory is specified
+    if (-d $outdir) {$wd = $outdir;}
+    else {die "Specified output directory \"$outdir\" does not exist"}
+    if ($verbose) {print "Working directory provided: $outdir\n";}
+}
+
+
+#Check if simv was specified and exists
+if ($simv) {
+    #An existing simv is specified, use that for simulation
+    if ($outdir) {
+        if (! -e catfile($outdir, $simv)) {die "Specified simv in output directory \"$outdir/$simv\" does not exist";}
+    }
+    else {        
+        if (! -e $simv) {die "Specified simv \"$simv\" does not exist";}
+    }
+}
+
+#If verdi and output directory specified, skip compile and run verdi directly, 
+if ($verdi && $fsdb) {
+    #check if fsdb file exists
+    my $verdi_com;
+    if ($outdir) {
+        if (! -e catfile($outdir, $fsdb)) {die "ERROR: Can't open FSDB file \"$outdir/$fsdb\"";}
+        if ($simv) {$verdi_com = "vcs verdi3 verdi -nologo -ssf $outdir/$fsdb -dbdir $outdir/${simv}.daidir &";}
+        else {$verdi_com = "vcs verdi3 verdi -nologo -ssf $outdir/$fsdb -dbdir $outdir/simv.daidir &";}
+    }
+    else {
+        if (! -e $fsdb) {die "ERROR: Can't open FSDB file \"$fsdb\"";}
+        if ($simv) {$verdi_com = "vcs verdi3 verdi -nologo -ssf $fsdb -dbdir ${simv}.daidir &";}
+        else {$verdi_com = "vcs verdi3 verdi -nologo -ssf $fsdb -dbdir simv.daidir &";}
+    }
+    if ($verbose) {print("$verdi_com\n");}
+    system($verdi_com)==0 or die "ERROR: verdi failed\n";
+    exit 0;
+}
+
+#Figure out cosimulation specification
+my $cosim;
+my $gls_dir;
+my $env_cosim="{subcells,prs,csp-standard.attributes.csp_model{csp}}";
+if ($level eq "hi") {
+    $cosim= "{csp,subcells,prs-standard.attributes.prs_model{subcells,prs,csp}}";
+}
+elsif ($level eq "lo") {
+    $cosim= "{verilog.rtl,subcells,prs,csp-standard.attributes.csp_model{csp}-standard.attributes.verilog_model{verilog.rtl,verilog.gate,subcells,prs}-standard.attributes.prs_model{subcells,prs,csp}}";
+}
+elsif ($level eq "fpga") {
+    $gls_dir= "$ENV{VERILOG_DIR}/fpga";
+    $cosim= "{verilog.rtl,verilog.gate,subcells,prs,csp-standard.attributes.csp_model{csp,subcells,prs}-standard.attributes.verilog_model{verilog.rtl,verilog.gate,subcells,prs}}";
+}
+elsif ($level eq "syn") {
+    $gls_dir= "$ENV{VERILOG_DIR}/syn";
+    $cosim= "{verilog.rtl,verilog.gate,subcells,prs,csp-standard.attributes.csp_model{csp,subcells,prs}-standard.attributes.verilog_model{verilog.rtl,verilog.gate,subcells,prs}}";
+}
+elsif ($level eq "gls" || $level eq "sdf") {
+    $gls_dir= "$ENV{VERILOG_DIR}";
+    $cosim= "{verilog.rtl,verilog.gate,subcells,prs,csp-standard.attributes.csp_model{csp,subcells,prs}-standard.attributes.verilog_model{verilog.rtl,verilog.gate,subcells,prs}}";
+    if ($level eq "sdf" && !$corner) {
+        print "WARNING: No corner specified for sdf simulation. Default corner \"max\" will be used\n";
+        $corner= "max";
+    }
+}
+else { die "ERROR: Unrecognized level $level. Must be {\"hi\", \"lo\", \"fpga\", \"syn\", \"gls\", \"sdf\"}"; }
+
+if ($verbose) {print "Cell=$cell, Cosim=$cosim\n";}
+my $cosim_spec = "$cell$cosim:$env";
+
+
+#Check if tcl file was specified and exists
+if ($tcl) {
+    #An existing tcl file is specified, use that for simulation
+    if (-e $tcl) {$tcl = abs_path($tcl);}
+    else {die "Specified simv \"$tcl\" does not exist";}
+}
+
+#If output directory was not specified, create one
+if (!$outdir) {
+    my $curtime = `workweek --format="%Y%m%d_%H%M"`;
+    chomp($curtime);
+    my $cell_base = (split(/\./, $cell))[-1];
+    if (looks_like_number($cell_base)) {
+        $cell_base = (split(/\./, $cell))[-2];
+    }
+    $wd = "vcs.${cell_base}_${env}.${level}.${curtime}";
+    mkdir $wd;
+    chmod 0750, $wd;
+    system("rm -if vcs_latest; ln -s $wd vcs_latest") == 0 or die "Symlink failed\n";
+}
+if ($verbose) {print "Changing to working directory: $wd\n";}
+chdir($wd) or die "$!";
+
+#If no simv was specified, generate verilog and compile it
+if (!$simv) {
+    #No simv specified, will need to generate one
+    #First: generate the verilog model
+    my $fulcrum = "fulcrum";
+    $fulcrum = $ENV{MY_FULCRUM} if (defined($ENV{MY_FULCRUM}));
+    my $gen_cmd="$fulcrum --latest gen_model --width=1024 --beh --kdb " .
+        "--cast-path=\"$ENV{CAST_PATH}\" " .
+        "--spar-dir=\"$ENV{SPAR_DIR}\" " .
+        "--cosim=\"$cosim_spec\" " .
+        join('', map { "\Q$_\E " } @gen_args) .
+        join('', map { "--define=\Q$_\E " } @cast_defines) .
+        join('', map { "--vcs-args=\Q$_\E " } @vcs_args) .
+        join('', map { "--c2v-args=\Q$_\E " } @c2v_args) .
+        "--define=standard.attributes.bd_controller.ExtraTimingMargin:10000 " .
+        "--define=standard.attributes.standard_cell.USE_LIBERTY_PRS:true " ;
+    if ($gls_dir) {
+        $gen_cmd .= "--gls-dir=$gls_dir ";
+        if ($level eq "sdf") {
+            $gen_cmd .= "--sdf=$corner ";
+        }
+    }
+    print "Running gen_model...\n";
+    if ($verbose) {print("$gen_cmd\n");}
+    system($gen_cmd)==0 or die "ERROR: gen_model failed in $wd\n";
+
+    #Second: compile the verilog model to a simv file
+    print "Running run-vcs...\n";
+    my $com_cmd = "verdi3 vcs ./run-vcs.sh >& vcs.log";
+    if ($verbose) {print("$com_cmd\n");}
+    system($com_cmd)==0 or die "ERROR: Compiling model failed in $wd\n";
+
+    $simv = "./simv";
+}
+
+if (!$t_run) {
+    print "WARNING: No run time specified, defaulting to 50us\n";
+    $t_run = "50us";
+}
+
+if (!$fsdb) {
+    print "WARNING: No FSDB file specified, defaulting to trace.fsdb\n";
+    $fsdb = "trace.fsdb";
+}
+
+#If no tcl file was specified, create default one
+if (!$tcl) {
+    $tcl = "./run.tcl";
+    open TCL, ">$tcl" or die;
+    print TCL <<EOF;
+fsdbDumpfile $fsdb
+fsdbDumpvars 0 TESTBENCH +all
+run ${t_run}
+exit
+EOF
+}
+
+#Run the simulation
+print "Running simulation...\n";
+my $sim_cmd = "verdi3 $simv -fgp=num_threads:4 +vcs+initreg+0 -ucli -do $tcl @simv_args >& sim.log";
+print("$sim_cmd\n");
+system($sim_cmd)==0 or die "ERROR: Simulation failed in $wd\n";
+
+#if verdi is specified after compile and simulation, run verdi in the output directory
+if ($verdi) {
+    print "Running Verdi...\n";
+    my $verdi_com = "vcs verdi3 verdi -nologo -ssf $fsdb -dbdir ${simv}.daidir &";
+    if ($verbose) {print("$verdi_com\n");}
+    system($verdi_com)==0 or die "ERROR: verdi failed\n";
+    exit 0;
+}
+
+__END__
+=head1 NAME
+
+sim_block.pl - Wrapper for compiling and running Verilog VCS models
+
+=head1 SYNOPSIS
+
+sim_block.pl [options]
+
+ Options:
+   --cell          Name of the CAST cell to simulate
+   --env           Name of the CAST environment
+   --level         The level of simulation {"hi", "lo", "fpga", "syn", "gls", "sdf"}
+   --corner        Select which SDF file to use (if running sdf level simulation). Default is "max"
+   --outdir        Output directory for simulation files
+   --simv          Specify the simv file to execute (skip compiling a new one)
+   --tcl           Specify the tcl file to run. Defaults to run.tcl
+   --t-run         Specify the run time. Defaults to 50us
+   --verdi         Run verdi after simulation (or open an existing FSDB)
+   --fsdb          Specify the name of the FSDB file. Defaults to trace.fsdb
+   --define        Override CAST variables; can be specified any number of times
+   --c2v-args      Flags to cast2verilog; can be specified any number of times
+   --vcs-args      Flags to VCS; can be specified any number of times
+   --vcs-args      Flags to gen_model; can be specified any number of times
+   --simv-args     Flags to simv; can be specified any number of times
+   --help          Prints this help message
+=cut
