2976bc951f5e19cac8e438a4f80afc2564650814
Karmazin, Robert <robert.karmazin@intel.com>
2025-04-01 09:28:29 -0800

    Performance history monitors for PROBE, ntvsram
    [git-p4: depot-paths = "//mrl/sw/intel/": change = 371220]

--- async-toolkit/jtools/cad/java/src/com/avlsi/csp/csp2verilog/runtime/bd_hist_mon.sv
+++ async-toolkit/jtools/cad/java/src/com/avlsi/csp/csp2verilog/runtime/bd_hist_mon.sv
@@ -402,6 +402,150 @@ end
 
 endmodule
 
+module bd_probe_hist_mon (
+  input logic rb ,
+  input logic \A.0 ,
+  input logic \A.1 ,
+  input logic \A.e ,
+  input logic \L.0 ,
+  input logic \L.e ,
+  input logic \Q.0 ,
+  input logic \Q.1 ,
+  input logic \Q.e );
+
+realtime t_Ae, t_L0, t_Q0, t_Q1;
+int n_Ae, n_L0, n_Q0, n_Q1;
+string ModName;
+
+initial begin
+  //Register ModName name
+  t_Ae = '0;
+  t_L0 = '0;
+  t_Q0 = '0;
+  t_Q1 = '0;
+  n_Ae = '0;
+  n_L0 = '0;
+  n_Q0 = '0;
+  n_Q1 = '0;
+  ModName = $sformatf("%m");
+  $timeformat(-15,0,"fs");
+  histmon_register_node({ModName,".\\A.0 "});
+  histmon_register_node({ModName,".\\A.1 "});
+  histmon_register_node({ModName,".\\L.e "});
+  histmon_register_node({ModName,".\\Q.e "});
+end
+
+always @( \A.e ) begin
+  t_Ae = (rb==1'b0) ? 1'b0 : $realtime;
+  n_Ae = (rb==1'b0) ? 1'b0 : n_Ae+1;
+end
+always @( \L.0 ) begin
+  t_L0 = (rb==1'b0) ? 1'b0 : $realtime;
+  n_L0 = (rb==1'b0) ? 1'b0 : n_L0+1;
+end
+always @( \Q.0 ) begin
+  t_Q0 = (rb==1'b0) ? 1'b0 : $realtime;
+  n_Q0 = (rb==1'b0) ? 1'b0 : n_Q0+1;
+end
+always @( \Q.1 ) begin
+  t_Q1 = (rb==1'b0) ? 1'b0 : $realtime;
+  n_Q1 = (rb==1'b0) ? 1'b0 : n_Q1+1;
+end
+
+always @( \A.0 ) begin
+  if (t_L0 > t_Ae && t_L0 > t_Q0 && t_L0 > t_Q1) begin
+    //$display("%m: A.0 fires @ %0t due to L[0] @ %0t", $realtime, t_L0);
+    histmon_add_transition({ModName,".\\A.0 "}, $realtime, {ModName,".\\L.0 "}, t_L0, "probe_mon");
+  end
+  else if (t_Ae > t_L0 && t_Ae > t_Q0 && t_Ae > t_Q1) begin
+    //$display("%m: A.0 fires @ %0t due to A.e @ %0t", $realtime, t_Ae);
+    histmon_add_transition({ModName,".\\A.0 "}, $realtime, {ModName,".\\A.e "}, t_Ae, "probe_mon");
+  end
+  else if (t_Q0 > t_L0 && t_Q0 > t_Ae && t_Q0 > t_Q1) begin
+    //$display("%m: A.0 fires @ %0t due to Q[0] @ %0t", $realtime, t_Q0);
+    histmon_add_transition({ModName,".\\A.0 "}, $realtime, {ModName,".\\Q.0 "}, t_Q0, "probe_mon");
+  end
+  else if (t_Q1 > t_L0 && t_Q1 > t_Ae && t_Q1 > t_Q0) begin
+    //$display("%m: A.0 fires @ %0t due to Q[1] @ %0t", $realtime, t_Q1);
+    histmon_add_transition({ModName,".\\A.0 "}, $realtime, {ModName,".\\Q.1 "}, t_Q1, "probe_mon");
+  end
+  else begin
+    $display("%m: A.0 fires @ %0t but could not determine causality", $realtime);
+  end
+end
+
+always @( \A.1 ) begin
+  if (t_L0 > t_Ae && t_L0 > t_Q0 && t_L0 > t_Q1) begin
+    //$display("%m: A.1 fires @ %0t due to L[0] @ %0t", $realtime, t_L0);
+    histmon_add_transition({ModName,".\\A.1 "}, $realtime, {ModName,".\\L.0 "}, t_L0, "probe_mon");
+  end
+  else if (t_Ae > t_L0 && t_Ae > t_Q0 && t_Ae > t_Q1) begin
+    //$display("%m: A.1 fires @ %0t due to A.e @ %0t", $realtime, t_Ae);
+    histmon_add_transition({ModName,".\\A.1 "}, $realtime, {ModName,".\\A.e "}, t_Ae, "probe_mon");
+  end
+  else if (t_Q0 > t_L0 && t_Q0 > t_Ae && t_Q0 > t_Q1) begin
+    //$display("%m: A.1 fires @ %0t due to Q[0] @ %0t", $realtime, t_Q0);
+    histmon_add_transition({ModName,".\\A.1 "}, $realtime, {ModName,".\\Q.0 "}, t_Q0, "probe_mon");
+  end
+  else if (t_Q1 > t_L0 && t_Q1 > t_Ae && t_Q1 > t_Q0) begin
+    //$display("%m: A.1 fires @ %0t due to Q.1 @ %0t", $realtime, t_Q1);
+    histmon_add_transition({ModName,".\\A.1 "}, $realtime, {ModName,".\\Q.1 "}, t_Q1, "probe_mon");
+  end
+  else begin
+    $display("%m: A.1 fires @ %0t but could not determine causality", $realtime);
+  end
+end
+
+always @( \L.e ) begin
+  if (t_L0 > t_Ae && t_L0 > t_Q0 && t_L0 > t_Q1) begin
+    //$display("%m: L.e fires @ %0t due to L[0] @ %0t", $realtime, t_L0);
+    histmon_add_transition({ModName,".\\L.e "}, $realtime, {ModName,".\\L.0 "}, t_L0, "probe_mon");
+  end
+  else if (t_Ae > t_L0 && t_Ae > t_Q0 && t_Ae > t_Q1) begin
+    //$display("%m: L.e fires @ %0t due to A.e @ %0t", $realtime, t_Ae);
+    histmon_add_transition({ModName,".\\L.e "}, $realtime, {ModName,".\\A.e "}, t_Ae, "probe_mon");
+  end
+  else if (t_Q0 > t_L0 && t_Q0 > t_Ae && t_Q0 > t_Q1) begin
+    //$display("%m: L.e fires @ %0t due to Q[0] @ %0t", $realtime, t_Q0);
+    histmon_add_transition({ModName,".\\L.e "}, $realtime, {ModName,".\\Q.0 "}, t_Q0, "probe_mon");
+  end
+  else if (t_Q1 > t_L0 && t_Q1 > t_Ae && t_Q1 > t_Q0) begin
+    //$display("%m: L.e fires @ %0t due to Q[1] @ %0t", $realtime, t_Q1);
+    histmon_add_transition({ModName,".\\L.e "}, $realtime, {ModName,".\\Q.1 "}, t_Q1, "probe_mon");
+  end
+  else begin
+    $display("%m: L.e fires @ %0t but could not determine causality", $realtime);
+  end
+end
+
+always @( \Q.e ) begin
+  if (t_L0 > t_Ae && t_L0 > t_Q0 && t_L0 > t_Q1) begin
+    //$display("%m: Q.e fires @ %0t due to L[0] @ %0t", $realtime, t_L0);
+    histmon_add_transition({ModName,".\\Q.e "}, $realtime, {ModName,".\\L.0 "}, t_L0, "probe_mon");
+  end
+  else if (t_Ae > t_L0 && t_Ae > t_Q0 && t_Ae > t_Q1) begin
+    //$display("%m: Q.e fires @ %0t due to A.e @ %0t", $realtime, t_Ae);
+    histmon_add_transition({ModName,".\\Q.e "}, $realtime, {ModName,".\\A.e "}, t_Ae, "probe_mon");
+  end
+  else if (t_Q0 > t_L0 && t_Q0 > t_Ae && t_Q0 > t_Q1) begin
+    //$display("%m: Q.e fires @ %0t due to Q[0] @ %0t", $realtime, t_Q0);
+    histmon_add_transition({ModName,".\\Q.e "}, $realtime, {ModName,".\\Q.0 "}, t_Q0, "probe_mon");
+  end
+  else if (t_Q1 > t_L0 && t_Q1 > t_Ae && t_Q1 > t_Q0) begin
+    //$display("%m: Q.e fires @ %0t due to Q[1] @ %0t", $realtime, t_Q1);  
+    histmon_add_transition({ModName,".\\Q.e "}, $realtime, {ModName,".\\Q.1 "}, t_Q1, "probe_mon");
+  end
+  else begin
+    $display("%m: Q.e fires @ %0t but could not determine causality", $realtime);
+  end
+end
+
+final begin
+  histmon_sim_done();
+end
+
+endmodule
+
 module bd_buf1of2_hist_mon (
   input logic rb ,
   input logic \L.0 ,
@@ -679,5 +823,113 @@ final begin
   histmon_sim_done();
 end
 
+endmodule
+
+
+module bd_ntvsram_hist_mon (
+  input logic \IA.C.a ,
+  input logic \IA.C.q ,
+  input logic \R.C.a ,
+  input logic \R.C.q ,
+  input logic \W.C.a ,
+  input logic \W.C.q );
+
+realtime t_IA_C_q, t_R_C_a, t_W_C_q;
+int n_IA_C_q, n_R_C_a, n_W_C_q;
+string ModName;
+
+initial begin
+  t_IA_C_q = '0;
+  t_R_C_a = '0;
+  t_W_C_q = '0;
+  n_IA_C_q = '0;
+  n_R_C_a = '0;
+  n_W_C_q = '0;
+  ModName = $sformatf("%m");
+  $timeformat(-15,0,"fs");
+  histmon_register_node({ModName,"\\IA..a"});
+  histmon_register_node({ModName,"\\W.C.a"});
+  histmon_register_node({ModName,"\\R.C.q"});
+end
+
+always @( \IA.C.q ) begin
+  t_IA_C_q = ( \IA.C.a === 1'bx) ? 1'b0 : $realtime;
+  n_IA_C_q = ( \IA.C.a === 1'bx) ? '0 : n_IA_C_q+1;
+end
+always @( \R.C.a ) begin
+  t_R_C_a = ( \R.C.q === 1'bx) ? 1'b0 : $realtime;
+  n_R_C_a = ( \R.C.q === 1'bx) ? '0 : n_R_C_a+1;
+end
+always @( \W.C.q ) begin
+  t_W_C_q = ( \W.C.a === 1'bx) ? 1'b0 : $realtime;
+  n_W_C_q = ( \W.C.a === 1'bx) ? '0 : n_W_C_q+1;
+end
+
+always @( \IA.C.a ) begin
+  if (t_IA_C_q > t_R_C_a && t_IA_C_q > t_W_C_q) begin
+    //IA.C.q is the critical path
+    //$display("%m: IA.C.a fires @ %0t due to IA.C.q @ %0t", $realtime, t_IA_C_q);
+    histmon_add_transition({ModName,"\\IA.C.a"}, $realtime, {ModName,"\\IA.C.q"}, t_IA_C_q, "ntvsram_mon");
+  end
+  else if (t_R_C_a > t_IA_C_q && t_R_C_a > t_W_C_q) begin
+    //R.C.a is the critical path
+    //$display("%m: IA.C.a fires @ %0t due to R.C.a @ %0t", $realtime, t_R_C_a);
+    histmon_add_transition({ModName,"\\IA.C.a"}, $realtime, {ModName,"\\R.C.a"}, t_R_C_a, "ntvsram_mon");
+  end
+  else if (t_W_C_q > t_IA_C_q && t_W_C_q > t_R_C_a) begin
+    //W.C.q is the critical path
+    //$display("%m: IA.C.a fires @ %0t due to W.C.q @ %0t", $realtime, t_W_C_q);
+    histmon_add_transition({ModName,"\\IA.C.a"}, $realtime, {ModName,"\\W.C.q"}, t_W_C_q, "ntvsram_mon");
+  end
+  else begin
+    $display("%m: IA.C.a fires @ %0t but could not determine causality", $realtime);
+  end
+end
+
+always @( \R.C.q ) begin
+  if (t_IA_C_q > t_R_C_a && t_IA_C_q > t_W_C_q) begin
+    //IA.C.q is the critical path
+    //$display("%m: R.C.q fires @ %0t due to IA.C.q @ %0t", $realtime, t_IA_C_q);
+    histmon_add_transition({ModName,"\\R.C.q"}, $realtime, {ModName,"\\IA.C.q"}, t_IA_C_q, "ntvsram_mon");
+  end
+  else if (t_R_C_a > t_IA_C_q && t_R_C_a > t_W_C_q) begin
+    //R.C.a is the critical path    
+    //$display("%m: R.C.q fires @ %0t due to R.C.a @ %0t", $realtime, t_R_C_a);
+    histmon_add_transition({ModName,"\\R.C.q"}, $realtime, {ModName,"\\R.C.a"}, t_R_C_a, "ntvsram_mon");
+  end
+  else if (t_W_C_q > t_IA_C_q && t_W_C_q > t_R_C_a) begin
+    //W.C.q is the critical path
+    //$display("%m: R.C.q fires @ %0t due to W.C.q @ %0t", $realtime, t_W_C_q);
+    histmon_add_transition({ModName,"\\R.C.q"}, $realtime, {ModName,"\\W.C.q"}, t_W_C_q, "ntvsram_mon");
+  end
+  else begin
+    $display("%m: R.C.q fires @ %0t but could not determine causality", $realtime);
+  end
+end
+
+always @( \W.C.a ) begin
+  if (t_IA_C_q > t_R_C_a && t_IA_C_q > t_W_C_q) begin
+    //IA.C.q is the critical path
+    //$display("%m: W.C.a fires @ %0t due to IA.C.q @ %0t", $realtime, t_IA_C_q);
+    histmon_add_transition({ModName,"\\W.C.a"}, $realtime, {ModName,"\\IA.C.q"}, t_IA_C_q, "ntvsram_mon");
+  end
+  else if (t_R_C_a > t_IA_C_q && t_R_C_a > t_W_C_q) begin
+    //R.C.a is the critical path
+    //$display("%m: W.C.a fires @ %0t due to R.C.a @ %0t", $realtime, t_R_C_a);
+    histmon_add_transition({ModName,"\\W.C.a"}, $realtime, {ModName,"\\R.C.a"}, t_R_C_a, "ntvsram_mon");
+  end
+  else if (t_W_C_q > t_IA_C_q && t_W_C_q > t_R_C_a) begin
+    //W.C.q is the critical path
+    //$display("%m: W.C.a fires @ %0t due to W.C.q @ %0t", $realtime, t_W_C_q);
+    histmon_add_transition({ModName,"\\W.C.a"}, $realtime, {ModName,"\\W.C.q"}, t_W_C_q, "ntvsram_mon");
+  end
+  else begin
+    $display("%m: W.C.a fires @ %0t but could not determine causality", $realtime);
+  end
+end
+
+final begin
+  histmon_sim_done();
+end
 
 endmodule
