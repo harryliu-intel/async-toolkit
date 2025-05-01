f39585be7929a4c2fe41626d07b63da4fedcfb82
Karmazin, Robert <robert.karmazin@intel.com>
2025-04-30 21:10:32 -0800

    Add power monitor module to the VCS flow. Logs transitions of a signal to a file
    [git-p4: depot-paths = "//mrl/sw/intel/": change = 371500]

---
+++ async-toolkit/jtools/cad/java/src/com/avlsi/csp/csp2verilog/runtime/power_mon.sv
@@ -0,0 +1,26 @@
+`timescale 1ps/1fs
+
+module power_mon #(string name) (input logic signal);
+
+realtime t_sig;
+logic prev_sig;
+int file;
+
+initial begin
+  file = $fopen($sformatf("power_%s.dat", name), "w");
+  $timeformat(-12,0,"ps");
+  prev_sig <= signal;
+end
+
+always @(signal) begin
+  if (prev_sig === 1'b0 || prev_sig === 1'b1) begin
+    $fdisplay(file, "%0t", $realtime);
+  end
+  prev_sig = signal;
+end
+
+final begin
+  $fclose(file);
+end
+
+endmodule
