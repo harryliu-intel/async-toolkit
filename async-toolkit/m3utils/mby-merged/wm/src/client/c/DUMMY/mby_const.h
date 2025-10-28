cb8fe828242ca822c9cd6e1f13051dfb64d39259
Mika Nystrom <mika.nystroem@intel.com>
2025-10-28 12:31:03 -0700

    add dummies here too

---
+++ async-toolkit/m3utils/mby-merged/wm/src/client/c/DUMMY/mby_const.h
@@ -0,0 +1,15 @@
+#ifndef _mby_const_H
+#define _mby_const_H
+
+#include "uint.h"
+
+
+#define MBY_PA_MAX_SEG_LEN    192
+
+#define MBY_N_PARSER_KEYS    80
+
+#define MBY_N_PARSER_PTRS    8
+
+#define MBY_N_PARSER_FLAGS    48
+
+#endif /* !_mby_const_H */
---
+++ async-toolkit/m3utils/mby-merged/wm/src/client/c/DUMMY/mby_parserHdrPtrs.h
@@ -0,0 +1,21 @@
+#ifndef _mby_parserHdrPtrs_H
+#define _mby_parserHdrPtrs_H
+
+#include "uint.h"
+
+#include "mby_const.h"
+
+typedef struct {
+  uint8                    OFFSET                  [MBY_N_PARSER_PTRS];
+  fm_bool                  OFFSET_VALID            [MBY_N_PARSER_PTRS];
+  uint8                    PROT_ID                 [MBY_N_PARSER_PTRS];
+} mby_parserHdrPtrs;
+
+void mby_parserHdrPtrs_serialize(uint64 *s, const mby_parserHdrPtrs *t);
+
+void mby_parserHdrPtrs_deserialize(const uint64 *s, mby_parserHdrPtrs *t);
+
+#define mby_parserHdrPtrs_serial_qwords 24
+typedef uint64 mby_parserHdrPtrs_serial_t[mby_parserHdrPtrs_serial_qwords];
+
+#endif /* !_mby_parserHdrPtrs_H */
