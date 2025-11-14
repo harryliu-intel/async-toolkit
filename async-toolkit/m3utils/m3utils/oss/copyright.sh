57d680afbc6f2fbb2f2a57b57608808ca39a1ffd
Mika Nystrom <mika.nystroem@intel.com>
2025-11-14 13:30:25 -0800

    add -r so it runs zero times for empty input

--- async-toolkit/m3utils/m3utils/oss/copyright.sh
+++ async-toolkit/m3utils/m3utils/oss/copyright.sh
@@ -5,11 +5,11 @@ LINE1="Copyright (c) %Y Intel Corporation.  All rights reserved.  See the file C
 LINE2="SPDX-License-Identifier: Apache-2.0"
 M3INSTALL=/nfs/pdx/disks/or_n3a_disk001/w138/mnystroe/m3utils
 
-(find . -type f -and -name \*.\* | xargs -n1 ${M3INSTALL}/copyright/AMD64_LINUX/copyright -year 2025 -line1 "${LINE1}" -line2 "${LINE2}" ) 
+(find . -type f -and -name \*.\* | xargs -r -n1 ${M3INSTALL}/copyright/AMD64_LINUX/copyright -year 2025 -line1 "${LINE1}" -line2 "${LINE2}" ) 
 
-(find . -type f -and -name m3makefile | xargs -n1 ${M3INSTALL}/copyright/AMD64_LINUX/copyright -type tex -year 2025 -line1 "${LINE1}" -line2 "${LINE2}" )
+(find . -type f -and -name m3makefile | xargs -r -n1 ${M3INSTALL}/copyright/AMD64_LINUX/copyright -type tex -year 2025 -line1 "${LINE1}" -line2 "${LINE2}" )
 
-(find . -type f -and -name Makefile | xargs -n1 ${M3INSTALL}/copyright/AMD64_LINUX/copyright -type sh -year 2025 -line1 "${LINE1}" -line2 "${LINE2}" )
+(find . -type f -and -name Makefile | xargs -r -n1 ${M3INSTALL}/copyright/AMD64_LINUX/copyright -type sh -year 2025 -line1 "${LINE1}" -line2 "${LINE2}" )
 
-(find . -type f -and -name GNUmakefile | xargs -n1 ${M3INSTALL}/copyright/AMD64_LINUX/copyright -type sh -year 2025 -line1 "${LINE1}" -line2 "${LINE2}" )
+(find . -type f -and -name GNUmakefile | xargs -r -n1 ${M3INSTALL}/copyright/AMD64_LINUX/copyright -type sh -year 2025 -line1 "${LINE1}" -line2 "${LINE2}" )
 
